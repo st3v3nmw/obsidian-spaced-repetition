95160100ba1d6d167bda599942a3d14b6919968c
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-01-31 15:47:50 +0300

    sm2 algorithm

--- main.ts
+++ main.ts
@@ -1,22 +1,21 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, Vault, addIcon, WorkspaceLeaf, iterateCacheRefs, getLinkpath } from 'obsidian';
+import { Notice, Plugin, addIcon, iterateCacheRefs, getLinkpath, ItemView } from 'obsidian';
 
-const SCHEDULING_INFO_REGEX = /<!--DUE: ([0-9]+), INTERVAL: ([0-9]+), EASE: ([.0-9]+)-->/;
+const SCHEDULING_INFO_REGEX = /<!--DUE: ([0-9]+), INTERVAL: ([0-9]+), EASE: ([0-9]+), READABLE: ([0-z ]+)-->/;
 
 export default class MyPlugin extends Plugin {
+	private overdue_notes: [];
+	private statusBar;
+
 	async onload() {
 		console.log("loading Notes Review plugin...");''
 
-		// addIcon('libraryreview', `<path fill="white" stroke="white" d="m328.853 56.033c-38.598 0-70 31.402-70 70s31.402 70 70 70 70-31.402 70-70-31.402-70-70-70zm0 120c-27.57 0-50-22.43-50-50s22.43-50 50-50 50 22.43 50 50-22.43 50-50 50z"/><path fill="white" stroke="white" d="m502.924 253.962-76.735-76.735c8.08-15.3 12.664-32.72 12.664-51.193 0-60.654-49.346-110-110-110s-110 49.346-110 110c0 26.569 9.469 50.968 25.209 70h-203.996c-3.028 0-5.893 1.372-7.791 3.73-16.367 20.338-16.367 52.201 0 72.539 1.898 2.358 4.763 3.73 7.791 3.73h23.282c-4.449 12.792-4.449 27.208 0 40h-43.282c-3.028 0-5.893 1.372-7.791 3.73-16.367 20.338-16.367 52.201 0 72.539 1.898 2.358 4.763 3.73 7.791 3.73h13.282c-6.491 18.663-3.523 40.799 8.928 56.27 1.898 2.358 4.763 3.73 7.791 3.73h60v30c0 4.045 2.437 7.691 6.173 9.239 3.737 1.546 8.038.692 10.898-2.168l22.929-22.929 22.929 22.929c1.914 1.913 4.471 2.929 7.074 2.929 1.288 0 2.587-.249 3.824-.761 3.737-1.548 6.173-5.194 6.173-9.239v-30h150c5.523 0 10-4.478 10-10s-4.477-10-10-10h-4.782c-6.918-11.719-6.918-28.281 0-40h4.782c5.523 0 10-4.478 10-10s-4.477-10-10-10h-34.782c-6.918-11.719-6.918-28.281 0-40h64.782c5.523 0 10-4.478 10-10s-4.477-10-10-10h-4.782c-6.918-11.719-6.918-28.281 0-40h4.782c5.523 0 10-4.478 10-10s-4.477-10-10-10h-44.768c-3.445-5.837-5.269-13.02-5.215-20.35 2.894.229 5.818.35 8.77.35 19.523 0 37.871-5.119 53.782-14.075l76.147 76.146c6.085 6.085 14.078 9.127 22.071 9.127s15.986-3.042 22.071-9.127c5.896-5.896 9.143-13.733 9.143-22.071s-3.249-16.175-9.144-22.07zm-332.858 207.928-12.929-12.929c-3.905-3.904-10.237-3.904-14.143 0l-12.929 12.929v-35.857h40v35.857zm143.282-25.857h-123.282v-10h10c5.523 0 10-4.478 10-10s-4.477-10-10-10h-100c-5.523 0-10 4.478-10 10s4.477 10 10 10h10v10h-54.785c-6.921-11.724-6.894-28.281.034-40h258.033c-4.449 12.791-4.449 27.208 0 40zm-30-60h-258.067c-6.918-11.719-6.918-28.281 0-40h258.067c-4.449 12.791-4.449 27.208 0 40zm60-60h-258.064c-6.918-11.719-6.918-28.281 0-40h258.064c-4.449 12.791-4.449 27.208 0 40zm-39.984-60h-258.083c-6.918-11.719-6.918-28.281 0-40h220.4c10.342 7.28 21.988 12.822 34.514 16.206-.505 8.191.584 16.326 3.169 23.794zm-64.511-130c0-49.626 40.374-90 90-90s90 40.374 90 90-40.374 90-90 90-90-40.374-90-90zm249.928 157.929c-4.371 4.373-11.485 4.373-15.857 0l-73.566-73.565c5.745-4.809 10.993-10.191 15.655-16.06l73.768 73.768c2.118 2.118 3.284 4.934 3.284 7.929s-1.166 5.81-3.284 7.928z"/>`);
+		addIcon('crosshairs', `<path style=" stroke:none;fill-rule:nonzero;fill:currentColor;fill-opacity:1;" d="M 99.921875 47.941406 L 93.074219 47.941406 C 92.84375 42.03125 91.390625 36.238281 88.800781 30.921875 L 85.367188 32.582031 C 87.667969 37.355469 88.964844 42.550781 89.183594 47.84375 L 82.238281 47.84375 C 82.097656 44.617188 81.589844 41.417969 80.734375 38.304688 L 77.050781 39.335938 C 77.808594 42.089844 78.261719 44.917969 78.40625 47.769531 L 65.871094 47.769531 C 64.914062 40.507812 59.144531 34.832031 51.871094 33.996094 L 51.871094 21.386719 C 54.816406 21.507812 57.742188 21.960938 60.585938 22.738281 L 61.617188 19.058594 C 58.4375 18.191406 55.164062 17.691406 51.871094 17.570312 L 51.871094 10.550781 C 57.164062 10.769531 62.355469 12.066406 67.132812 14.363281 L 68.789062 10.929688 C 63.5 8.382812 57.738281 6.953125 51.871094 6.734375 L 51.871094 0.0390625 L 48.054688 0.0390625 L 48.054688 6.734375 C 42.179688 6.976562 36.417969 8.433594 31.132812 11.007812 L 32.792969 14.441406 C 37.566406 12.140625 42.761719 10.84375 48.054688 10.625 L 48.054688 17.570312 C 44.828125 17.714844 41.628906 18.21875 38.515625 19.078125 L 39.546875 22.757812 C 42.324219 21.988281 45.175781 21.53125 48.054688 21.386719 L 48.054688 34.03125 C 40.796875 34.949219 35.089844 40.679688 34.203125 47.941406 L 21.5 47.941406 C 21.632812 45.042969 22.089844 42.171875 22.855469 39.375 L 19.171875 38.34375 C 18.3125 41.457031 17.808594 44.65625 17.664062 47.882812 L 10.664062 47.882812 C 10.882812 42.589844 12.179688 37.394531 14.480469 32.621094 L 11.121094 30.921875 C 8.535156 36.238281 7.078125 42.03125 6.847656 47.941406 L 0 47.941406 L 0 51.753906 L 6.847656 51.753906 C 7.089844 57.636719 8.542969 63.402344 11.121094 68.695312 L 14.554688 67.035156 C 12.257812 62.261719 10.957031 57.066406 10.738281 51.773438 L 17.742188 51.773438 C 17.855469 55.042969 18.34375 58.289062 19.191406 61.445312 L 22.871094 60.414062 C 22.089844 57.5625 21.628906 54.632812 21.5 51.679688 L 34.203125 51.679688 C 35.058594 58.96875 40.773438 64.738281 48.054688 65.660156 L 48.054688 78.308594 C 45.105469 78.1875 42.183594 77.730469 39.335938 76.957031 L 38.304688 80.636719 C 41.488281 81.511719 44.757812 82.015625 48.054688 82.144531 L 48.054688 89.144531 C 42.761719 88.925781 37.566406 87.628906 32.792969 85.328125 L 31.132812 88.765625 C 36.425781 91.3125 42.183594 92.742188 48.054688 92.960938 L 48.054688 99.960938 L 51.871094 99.960938 L 51.871094 92.960938 C 57.75 92.71875 63.519531 91.265625 68.808594 88.6875 L 67.132812 85.253906 C 62.355469 87.550781 57.164062 88.851562 51.871094 89.070312 L 51.871094 82.125 C 55.09375 81.980469 58.292969 81.476562 61.40625 80.617188 L 60.378906 76.9375 C 57.574219 77.703125 54.695312 78.15625 51.792969 78.289062 L 51.792969 65.679688 C 59.121094 64.828125 64.910156 59.0625 65.796875 51.734375 L 78.367188 51.734375 C 78.25 54.734375 77.789062 57.710938 76.992188 60.605469 L 80.675781 61.636719 C 81.558594 58.40625 82.066406 55.082031 82.183594 51.734375 L 89.261719 51.734375 C 89.042969 57.03125 87.742188 62.222656 85.445312 66.996094 L 88.878906 68.65625 C 91.457031 63.367188 92.910156 57.597656 93.152344 51.71875 L 100 51.71875 Z M 62.019531 51.734375 C 61.183594 56.945312 57.085938 61.023438 51.871094 61.828125 L 51.871094 57.515625 L 48.054688 57.515625 L 48.054688 61.808594 C 42.910156 60.949219 38.886719 56.902344 38.058594 51.753906 L 42.332031 51.753906 L 42.332031 47.941406 L 38.058594 47.941406 C 38.886719 42.789062 42.910156 38.746094 48.054688 37.886719 L 48.054688 42.179688 L 51.871094 42.179688 L 51.871094 37.847656 C 57.078125 38.648438 61.179688 42.71875 62.019531 47.921875 L 57.707031 47.921875 L 57.707031 51.734375 Z M 62.019531 51.734375 "/>`);
 
-		this.new_notes = [];
 		this.overdue_notes = [];
 		this.statusBar = this.addStatusBarItem();
-		await this.getNotesToReview();
-
-		this.addRibbonIcon('dice', 'Review Note', async () => {
-			await this.getNotesToReview();
-
-			if (this.overdue_notes.length + this.new_notes.length == 0) {
+		this.statusBar.classList.add("mod-clickable");
+		this.statusBar.addEventListener("click", _ => {
+			if (this.overdue_notes.length == 0) {
 				new Notice("You're done for the day :D.");
 				return;
 			}
@@ -24,57 +23,45 @@ export default class MyPlugin extends Plugin {
 			if (this.overdue_notes.length > 0) {
 				let cNote = this.overdue_notes[0];
 				for (let note of this.overdue_notes) {
-					if (note['due_unix'] > cNote['due_unix'])
-						cNote = note;
-				}
-				this.app.workspace.activeLeaf.openFile(cNote['note']);
-				return;
-			}
-
-			if (this.new_notes.length > 0) {
-				let cNote = this.new_notes[0];
-				for (let note of this.new_notes) {
-					if (note['due_unix'] > cNote['due_unix'])
+					if (note['due_unix'] < cNote['due_unix'])
 						cNote = note;
 				}
 				this.app.workspace.activeLeaf.openFile(cNote['note']);
 			}
 		});
 
-		this.addCommand({
-			id: 'mark-as-reviewed-easy',
-			name: 'Easy',
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						this.saveReviewResponse(1);
-						new Notice("Response received.");
-					}
-					return true;
-				}
-				return false;
-			}
+		this.addRibbonIcon('crosshairs', 'Sync', async () => {
+			await this.sync();
 		});
 
-		this.addCommand({
-			id: 'mark-as-reviewed-hard',
-			name: 'Hard',
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						this.saveReviewResponse(0);
-						new Notice("Response received.");
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+		this.app.workspace.on("file-menu", ((menu, file: TFile) => {
+			menu.addItem((item) =>{
+				item.setTitle("Note Review: Easy").setIcon("crosshairs")
+					.onClick(evt => {
+						this.saveReviewResponse(file, 1);
+				});
+			})
+		}));
+
+		this.app.workspace.on("file-menu", ((menu, file: TFile) => {
+			menu.addItem((item) =>{
+				item.setTitle("Note Review: Hard").setIcon("crosshairs")
+					.onClick(evt => {
+						this.saveReviewResponse(file, 0);
+				});
+			})
+		}));
+
+		await sleep(2000);
+		await this.sync();
 	}
 
-	async getNotesToReview() {
+	async onunload() {
+		await this.sync();
+		console.log("unloading Notes Review plugin...");
+	}
+
+	async sync() {
 		let notes = this.app.vault.getMarkdownFiles();
 		let scheduled_notes = {};
 		this.overdue_notes = [];
@@ -123,36 +110,53 @@ export default class MyPlugin extends Plugin {
 				}
 			}
 			let interval = 1;
-			let due_unix = +new Date + 24 * 3600 * 1000;
-			let ease = count == 0 ? 2.0 : (4 + total / count) / 3;
-			file_text = `<!--DUE: ${due_unix}, INTERVAL: ${interval}, EASE: ${ease}-->\n\n${new_note[1]}`;
+			let due = new Date((Math.floor(+new Date / (24 * 3600 * 1000)) + interval) * 24 * 3600 * 1000);
+			let ease = (count == 0 ? 250 : Math.floor((250 + total / count) / 2));
+			file_text = `<!--DUE: ${+due}, INTERVAL: ${interval}, EASE: ${ease}, READABLE: ${due.toDateString()}-->\n\n${new_note[1]}`;
 			this.app.vault.modify(new_note[0], file_text);
 		}
 
 		let now = +new Date();
 		for (let note in scheduled_notes) {
 			note = scheduled_notes[note];
-			if (Math.floor(note[1] / (24 * 3600 * 1000)) * 24 * 3600 * 1000 < now)
+			if (note[1] <= now)
 				this.overdue_notes.push({note: note[0], due_unix, interval, ease});
 		}
-		this.statusBar.setText(`Review: ${this.overdue_notes.length} due`);
+		this.statusBar.setText(`Review: ${this.overdue_notes.length} due, ${temp_new.length + Object.keys(scheduled_notes).length} total`);
+		new Notice("Notes review sync done.");
 	}
 
-	async saveReviewResponse(quality: int) {
-		let note = this.app.workspace.activeLeaf.view.file;
+	async saveReviewResponse(note: TFile, quality: Number) {
 		let file_text = await this.app.vault.read(note);
 
-		let interval = -1, ease = -1;
 		if (!/<!--IGNORE-->/.test(file_text)) { // checks if note should be ignored 
 			if (file_text.split(/\r\n|\r|\n/).length >= 3) { // file should have more than 3 or more lines
 				let scheduling_info = SCHEDULING_INFO_REGEX.exec(file_text);
-				interval = parseInt(scheduling_info[2]);
-				ease = parseFloat(scheduling_info[3]);
+				let interval = parseInt(scheduling_info[2]);
+				let ease = parseFloat(scheduling_info[3]);
+
+				ease = (quality == 1 ? ease + 20 : Math.max(130, ease - 20));
+				interval = Math.max(1, Math.floor(quality == 1 ? interval * ease / 100 : interval * 0.5));
+				if (interval >= 8) { // fuzz
+					let fuzz = Math.ceil(0.05 * interval);
+					let r = Math.random();
+					interval += (r < 0.33 ? -fuzz : (r < 0.67 ? 0 : fuzz));
+				}
+
+				let due = new Date((Math.floor(+new Date / (24 * 3600 * 1000)) + interval) * 24 * 3600 * 1000);
+				file_text = file_text.replace(SCHEDULING_INFO_REGEX, `<!--DUE: ${+due}, INTERVAL: ${interval}, EASE: ${ease}, READABLE: ${due.toDateString()}-->`);
+				this.app.vault.modify(note, file_text);
+
+				new Notice("Response received.");
+			} else {
+				new Notice("Note not scheduled yet.");
 			}
+		} else {
+			new Notice("Note marked as IGNORE.");
 		}
-		ease = Math.max(1, quality == 1 ? ease * 1.25 : ease * 0.8);
-		interval = Math.max(1, Math.floor(quality == 1 ? interval * ease : interval * 0.5));
-		file_text = file_text.replace(SCHEDULING_INFO_REGEX, `<!--DUE: ${(+new Date) + interval * 24 * 3600 * 1000}, INTERVAL: ${interval}, EASE: ${ease}-->`);
-		this.app.vault.modify(note, file_text);
 	}
+}
+
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
