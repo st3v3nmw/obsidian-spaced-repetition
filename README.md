f6f41469de266a7e8f822c4e0ae9d40b7718ae00
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-02-11 20:52:43 +0300

    right panel done

--- README.md
+++ README.md
@@ -4,15 +4,17 @@
 
 - Notes should be atomic i.e. focus on a single concept.
 - Outgoing links should point to concepts which the current note depends on or is highly related to (i.e. contrast, similarity, etc).
+- Reviews should start only after properly understanding a concept.
 
 ## Spaced Repetition Algorithm
 
+- Spaced repetition? [basics](https://ncase.me/remember/), [detailed](https://www.gwern.net/Spaced-repetition)
 - The algorithm is a variant of [Anki's algorithm](https://faqs.ankiweb.net/what-spaced-repetition-algorithm.html) which is based on the [SM-2 algorithm](https://www.supermemo.com/en/archives1990-2015/english/ol/sm2).
 - It supports binary reviews i.e. a concept is either hard or easy at the time of review.
 - initial ease is weighted depending on the average ease of outgoing links (o_factor), average ease of incoming links (i_factor), and the base ease.
   - The average ease of outgoing links contributes o_factor% to the initial ease
   - The average ease of incoming links contributes i_factor% to the initial ease
-  - By default, o_factor = 50, i_factor = 30, and base ease = 250 (contributes 20% to the initial ease)
+  - By default, o_factor = 50, i_factor = 25, and base ease = 250 (contributes 25% to the initial ease)
     - o_factor should be greater than i_factor
   - If we don't have either outgoing or incoming links, their respective weightings are allocated to the base ease
   - All values are changeable in settings
@@ -20,17 +22,16 @@
   - easy, the ease increases by 20 and the interval changes to `old_interval * new_ease / 100`
   - hard, the ease decreases by 20 and the interval changes to `old_interval * 0.5`
     - The 0.5 can be modified in settings
+    - Minimum ease = 130
   - For 8 or more days:
       - interval += random_choice({-fuzz, 0, +fuzz})
           - where fuzz = ceil(0.05 * interval)
-          - "[...] Anki also applies a small amount of random “fuzz” to prevent cards that were introduced at the same time and given the same ratings from sticking together and always coming up for review on the same day".
+          - Anki docs:
+            > "[...] Anki also applies a small amount of random “fuzz” to prevent cards that were introduced at the same time and given the same ratings from sticking together and always coming up for review on the same day."
 - Scheduling information stored in YAML front matter
 
 ## TODO
 
-- Right pane
-  - Groups items by dates (collapsible)
-  - Handle file renames and file open events
 - Fix workflow
 - Refactor code
 - Help & documentation
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { Notice, Plugin, addIcon, iterateCacheRefs, getLinkpath, ItemView, PluginSettingTab, Setting, WorkspaceLeaf } from 'obsidian';
 
 const SCHEDULING_INFO_REGEX = /^---\n((?:.*\n)*)due: ([0-9A-Za-z ]+)\ninterval: ([0-9]+)\nease: ([0-9]+)\n((?:.*\n)*)---/;
-const YAML_HEADER_REGEX = /^---\n((?:.*\n)*)---/;
+const YAML_FRONT_MATTER_REGEX = /^---\n((?:.*\n)*)---/;
 const IGNORE_REGEX = /review: ignore/;
 const DUE_DATES_VIEW_TYPE = 'due-dates-list-view';
 
@@ -16,7 +16,7 @@ interface ConceptsReviewSettings {
 const DEFAULT_SETTINGS: ConceptsReviewSettings = {
 	default_ease: 250,
 	o_factor: 0.5,
-	i_factor: 0.3,
+	i_factor: 0.25,
 	initial_interval: 1,
 	lapses_interval_change: 0.5
 }
@@ -29,8 +29,6 @@ export default class ConceptsReviewPlugin extends Plugin {
 	due_view: DueDatesListView;
 
 	async onload(): void {
-		console.log("loading Concepts Review plugin...");
-
 		await this.loadSettings();
 
 		addIcon('crosshairs', crossHairsIcon);
@@ -48,11 +46,11 @@ export default class ConceptsReviewPlugin extends Plugin {
 			new Notice("Sync done.");
 		});
 
-		this.registerView(DUE_DATES_VIEW_TYPE, (leaf) => (this.due_view = new DueDatesListView(leaf, this, this.scheduled_notes)));
+		this.registerView(DUE_DATES_VIEW_TYPE, (leaf) => this.due_view = new DueDatesListView(leaf, this, this.scheduled_notes));
 		if (this.app.workspace.layoutReady)
 			this.initView();
 		else
-			this.registerEvent(this.app.workspace.on('layout-ready', this.initView));
+			this.registerEvent(this.app.workspace.on('layout-ready', _ => this.initView()));
 
 		this.app.workspace.on("file-menu", ((menu, file: TFile) => {
 			menu.addItem((item) =>{
@@ -86,10 +84,6 @@ export default class ConceptsReviewPlugin extends Plugin {
 		await this.sync();
 	}
 
-	async onunload(): void {
-		console.log("unloading Concepts Review plugin...");
-	}
-
 	async sync(): void {
 		let notes = this.app.vault.getMarkdownFiles();
 		this.scheduled_notes = {};
@@ -97,19 +91,20 @@ export default class ConceptsReviewPlugin extends Plugin {
 
 		let links = {};
 		notes.forEach(n_file => {
-			links[n_file.path] = [];
+			links[n_file.path] = {"list": []};
 		});
 
 		notes.forEach(n_file => {
 			iterateCacheRefs(this.app.metadataCache.getFileCache(n_file), cb => {
 				let txt = this.app.metadataCache.getFirstLinkpathDest(getLinkpath(cb.link), n_file.path);
 				if (txt != null && txt.extension == "md") {
-					links[n_file.path].push([txt.path, "out"]);
-					links[txt.path].push([n_file.path, "in"]);
+					links[n_file.path]["list"].push([txt.path, "out"]);
+					links[txt.path]["list"].push([n_file.path, "in"]);
 				}
 			});
 		});
 		
+		let now = +new Date();
 		let temp_new = [];
 		for (let note of notes) {
 			let file_text = await this.app.vault.read(note);
@@ -124,24 +119,33 @@ export default class ConceptsReviewPlugin extends Plugin {
 				let due_unix = Date.parse(scheduling_info[2]);
 				let interval = parseInt(scheduling_info[3]);
 				let ease = parseInt(scheduling_info[4]);
-				this.scheduled_notes[note.path] = [note, due_unix, interval, ease];
+
+				if (!(due_unix in this.scheduled_notes))
+					this.scheduled_notes[due_unix] = {};
+				this.scheduled_notes[due_unix][note.path] = [note, due_unix, interval, ease];
+				links[note.path]["ease"] = ease;
+
+				if (due_unix <= now)
+					this.overdue_notes.push({note: note[0], due_unix: note[1], interval: note[2], ease: note[3]});
 			}
 		}
 
 		let interval = this.settings.initial_interval;
 		let due = new Date(+new Date + interval * 24 * 3600 * 1000);
+		let date_str = due.toDateString();
+		let due_unix = Date.parse(date_str); // cause timezones
 
 		for (let new_note of temp_new) {
 			let outgoing_link_total = 0, outgoing_link_count = 0;
 			let incoming_link_total = 0, incoming_link_count = 0;
-			for (let linked_file of links[new_note[0].path]) {
-				let q = this.scheduled_notes[linked_file[0]];
-				if (q) {
+			for (let linked_file of links[new_note[0].path]["list"]) {
+				let ease = links[linked_file[0]]["ease"];
+				if (ease) {
 					if (linked_file[1] == "out") {
-						outgoing_link_total += q[3];
+						outgoing_link_total += ease;
 						outgoing_link_count += 1;
 					} else {
-						incoming_link_total += q[3];
+						incoming_link_total += ease;
 						incoming_link_count += 1;
 					}
 				}
@@ -152,22 +156,24 @@ export default class ConceptsReviewPlugin extends Plugin {
 			initial_ease += (incoming_link_count > 0 ? this.settings.i_factor * incoming_link_total / incoming_link_count : this.settings.i_factor * this.settings.default_ease);
 			initial_ease = Math.floor(initial_ease);
 
-			if (YAML_HEADER_REGEX.test(new_note[1])) {
-				let info = YAML_HEADER_REGEX.exec(new_note[1]);
-				file_text = new_note[1].replace(YAML_HEADER_REGEX, `---\n${info[1]}due: ${due.toDateString()}\ninterval: ${interval}\nease: ${initial_ease}\n---`);
+			if (YAML_FRONT_MATTER_REGEX.test(new_note[1])) {
+				let info = YAML_FRONT_MATTER_REGEX.exec(new_note[1]);
+				file_text = new_note[1].replace(YAML_FRONT_MATTER_REGEX, `---\n${info[1]}due: ${date_str}\ninterval: ${interval}\nease: ${initial_ease}\n---`);
 			} else {
 				file_text = `---\ndue: ${due.toDateString()}\ninterval: ${interval}\nease: ${initial_ease}\n---\n\n${new_note[1]}`;
 			}
 			this.app.vault.modify(new_note[0], file_text);
-			this.scheduled_notes[new_note[0].path] = [new_note[0], +due, interval, initial_ease];
+
+			if (!(due_unix in this.scheduled_notes))
+				this.scheduled_notes[due_unix] = {};
+			this.scheduled_notes[due_unix][new_note.path] = [note, due_unix, interval, initial_ease];
 		}
 
-		let now = +new Date();
-		for (let note in this.scheduled_notes) {
-			note = this.scheduled_notes[note];
-			if (note[1] <= now)
-				this.overdue_notes.push({note: note[0], due_unix: note[1], interval: note[2], ease: note[3]});
-		}
+		this.scheduled_notes = Object.keys(this.scheduled_notes).sort().reduce((obj, key) => { 
+			obj[key] = this.scheduled_notes[key]; 
+			return obj;
+		}, {});
+		
 		this.statusBar.setText(`Review: ${this.overdue_notes.length} due, ${Object.keys(this.scheduled_notes).length} total`);
 		this.due_view.redraw();
 	}
@@ -242,9 +248,9 @@ function sleep(ms) {
 }
 
 function getFileLength(file_text: string) {
-	let yaml_header = YAML_HEADER_REGEX.exec(file_text);
-	let yaml_header_length = (yaml_header == null ? 0 : yaml_header[0].split(/\r\n|\r|\n/).length);
-	return file_text.split(/\r\n|\r|\n/).length - yaml_header_length;
+	let yaml_front_matter = YAML_FRONT_MATTER_REGEX.exec(file_text);
+	let yaml_front_matter_length = (yaml_front_matter == null ? 0 : yaml_front_matter[0].split(/\r\n|\r|\n/).length);
+	return file_text.split(/\r\n|\r|\n/).length - yaml_front_matter_length;
 }
 
 class ConceptsReviewSettingTab extends PluginSettingTab {
@@ -259,8 +265,9 @@ class ConceptsReviewSettingTab extends PluginSettingTab {
 		let {containerEl} = this;
 
 		containerEl.empty();
-		containerEl.createEl('h2', {text: 'Concepts Review - Settings'});
-		containerEl.createEl('a', {text: 'For more information check the wiki', href: `https://github.com/st3v3nmw/concepts-review/blob/master/README.md`});
+		containerEl.createEl('h2', {text: 'Concepts Review Plugin - Settings'});
+		let helpEl = containerEl.createEl('div', {cls: "help-div"});
+		helpEl.innerHTML = '<a href="https://github.com/st3v3nmw/concepts-review/blob/master/README.md">For more information, check the README.</a>';
 
 		new Setting(containerEl)
 			.setName('Base ease')
@@ -331,6 +338,9 @@ class DueDatesListView extends ItemView {
 		super(leaf);
 
 		this.plugin = plugin;
+		this.active_date = '';
+		this.registerEvent(this.app.workspace.on('file-open', _ => this.redraw()));
+		this.registerEvent(this.app.vault.on('rename', _ => this.redraw()));
 		this.redraw();
 	}
 
@@ -347,9 +357,8 @@ class DueDatesListView extends ItemView {
 	}
 
 	public onHeaderMenu(menu: Menu): void {
-		menu.addItem((item) => {
-			item
-				.setTitle('Close')
+		menu.addItem(item => {
+			item.setTitle('Close')
 				.setIcon('cross')
 				.onClick(() => {
 					this.app.workspace.detachLeavesOfType(DUE_DATES_VIEW_TYPE);
@@ -363,24 +372,53 @@ class DueDatesListView extends ItemView {
 		const rootEl = createDiv({cls: 'nav-folder mod-root'});
 		const childrenEl = rootEl.createDiv({cls: 'nav-folder-children'});
 
-		for (let currentFile in this.plugin.scheduled_notes) {
-			currentFile = this.plugin.scheduled_notes[currentFile];
-			const navFile = childrenEl.createDiv({cls: 'nav-file'});
-			const navFileTitle = navFile.createDiv({cls: 'nav-file-title'});
+		let now = +new Date();
+		let count = 0;
+		for (let due_unix in this.plugin.scheduled_notes) {
+			let due_on_date = this.plugin.scheduled_notes[due_unix];
+			let due = new Date(Number.parseInt(due_unix));
+			const dateFolderEl = childrenEl.createDiv({cls: 'nav-folder'});
+			const dateFolderTitleEl = dateFolderEl.createDiv({cls: 'nav-folder-title'});
+			const dateChildrenEl = dateFolderEl.createDiv({cls: 'nav-folder-children'});
+			const collapseIconEl = dateFolderTitleEl.createDiv({cls: 'nav-folder-collapse-indicator collapse-icon'});
+			collapseIconEl.innerHTML = collapseIcon;
 
-			if (openFile && currentFile[0].path === openFile.path) {
-				navFileTitle.addClass('is-active');
+			if (count > 0 || this.active_date == due_unix)
+				collapseIconEl.childNodes[0].style.transform = 'rotate(-90deg)';
+
+			let n_days = Math.ceil((due_unix - now) / (24 * 3600 * 1000));
+			dateFolderTitleEl.createDiv({cls: 'nav-folder-title-content', text: (n_days == -1 ? 'Yesterday' : (n_days == 0 ? 'Today' : n_days == 1 ? 'Tomorrow' : due.toDateString()))});
+
+			dateFolderTitleEl.onClickEvent(_ => {
+				for (let child of dateChildrenEl.childNodes) {
+					if (child.style.display == "block" || child.style.display == "") {
+						child.style.display = "none";
+						collapseIconEl.childNodes[0].style.transform = 'rotate(-90deg)';
+					} else {
+						child.style.display = "block";
+						collapseIconEl.childNodes[0].style.transform = '';
+					}
+				}
+			});
+		
+			for (let currentFile in due_on_date) {
+				currentFile = due_on_date[currentFile];
+
+				const navFile = dateChildrenEl.createDiv({cls: 'nav-file'});
+				if (count > 0 && this.active_date != due_unix)
+					navFile.style.display = "none";
+				
+				const navFileTitle = navFile.createDiv({cls: 'nav-file-title'});
+				if (openFile && currentFile[0].path === openFile.path)
+					navFileTitle.addClass('is-active');
+				navFileTitle.createDiv({cls: 'nav-file-title-content', text: currentFile[0].basename});
+				navFileTitle.onClickEvent(_ => {
+					this.app.workspace.activeLeaf.openFile(currentFile[0]);
+					this.active_date = due_unix;
+				});
 			}
 
-			let due = new Date(currentFile[1]);
-			navFileTitle.createDiv({
-				cls: 'nav-file-title-content',
-				text: `${currentFile[0].basename} - ${due.toDateString()}` 
-			});
-
-			navFile.onClickEvent(_ => {
-				this.app.workspace.activeLeaf.openFile(currentFile[0]);
-			});
+			count++;
 		}
 
 		const contentEl = this.containerEl.children[1];
@@ -390,3 +428,4 @@ class DueDatesListView extends ItemView {
 }
 
 const crossHairsIcon = `<path style=" stroke:none;fill-rule:nonzero;fill:currentColor;fill-opacity:1;" d="M 99.921875 47.941406 L 93.074219 47.941406 C 92.84375 42.03125 91.390625 36.238281 88.800781 30.921875 L 85.367188 32.582031 C 87.667969 37.355469 88.964844 42.550781 89.183594 47.84375 L 82.238281 47.84375 C 82.097656 44.617188 81.589844 41.417969 80.734375 38.304688 L 77.050781 39.335938 C 77.808594 42.089844 78.261719 44.917969 78.40625 47.769531 L 65.871094 47.769531 C 64.914062 40.507812 59.144531 34.832031 51.871094 33.996094 L 51.871094 21.386719 C 54.816406 21.507812 57.742188 21.960938 60.585938 22.738281 L 61.617188 19.058594 C 58.4375 18.191406 55.164062 17.691406 51.871094 17.570312 L 51.871094 10.550781 C 57.164062 10.769531 62.355469 12.066406 67.132812 14.363281 L 68.789062 10.929688 C 63.5 8.382812 57.738281 6.953125 51.871094 6.734375 L 51.871094 0.0390625 L 48.054688 0.0390625 L 48.054688 6.734375 C 42.179688 6.976562 36.417969 8.433594 31.132812 11.007812 L 32.792969 14.441406 C 37.566406 12.140625 42.761719 10.84375 48.054688 10.625 L 48.054688 17.570312 C 44.828125 17.714844 41.628906 18.21875 38.515625 19.078125 L 39.546875 22.757812 C 42.324219 21.988281 45.175781 21.53125 48.054688 21.386719 L 48.054688 34.03125 C 40.796875 34.949219 35.089844 40.679688 34.203125 47.941406 L 21.5 47.941406 C 21.632812 45.042969 22.089844 42.171875 22.855469 39.375 L 19.171875 38.34375 C 18.3125 41.457031 17.808594 44.65625 17.664062 47.882812 L 10.664062 47.882812 C 10.882812 42.589844 12.179688 37.394531 14.480469 32.621094 L 11.121094 30.921875 C 8.535156 36.238281 7.078125 42.03125 6.847656 47.941406 L 0 47.941406 L 0 51.753906 L 6.847656 51.753906 C 7.089844 57.636719 8.542969 63.402344 11.121094 68.695312 L 14.554688 67.035156 C 12.257812 62.261719 10.957031 57.066406 10.738281 51.773438 L 17.742188 51.773438 C 17.855469 55.042969 18.34375 58.289062 19.191406 61.445312 L 22.871094 60.414062 C 22.089844 57.5625 21.628906 54.632812 21.5 51.679688 L 34.203125 51.679688 C 35.058594 58.96875 40.773438 64.738281 48.054688 65.660156 L 48.054688 78.308594 C 45.105469 78.1875 42.183594 77.730469 39.335938 76.957031 L 38.304688 80.636719 C 41.488281 81.511719 44.757812 82.015625 48.054688 82.144531 L 48.054688 89.144531 C 42.761719 88.925781 37.566406 87.628906 32.792969 85.328125 L 31.132812 88.765625 C 36.425781 91.3125 42.183594 92.742188 48.054688 92.960938 L 48.054688 99.960938 L 51.871094 99.960938 L 51.871094 92.960938 C 57.75 92.71875 63.519531 91.265625 68.808594 88.6875 L 67.132812 85.253906 C 62.355469 87.550781 57.164062 88.851562 51.871094 89.070312 L 51.871094 82.125 C 55.09375 81.980469 58.292969 81.476562 61.40625 80.617188 L 60.378906 76.9375 C 57.574219 77.703125 54.695312 78.15625 51.792969 78.289062 L 51.792969 65.679688 C 59.121094 64.828125 64.910156 59.0625 65.796875 51.734375 L 78.367188 51.734375 C 78.25 54.734375 77.789062 57.710938 76.992188 60.605469 L 80.675781 61.636719 C 81.558594 58.40625 82.066406 55.082031 82.183594 51.734375 L 89.261719 51.734375 C 89.042969 57.03125 87.742188 62.222656 85.445312 66.996094 L 88.878906 68.65625 C 91.457031 63.367188 92.910156 57.597656 93.152344 51.71875 L 100 51.71875 Z M 62.019531 51.734375 C 61.183594 56.945312 57.085938 61.023438 51.871094 61.828125 L 51.871094 57.515625 L 48.054688 57.515625 L 48.054688 61.808594 C 42.910156 60.949219 38.886719 56.902344 38.058594 51.753906 L 42.332031 51.753906 L 42.332031 47.941406 L 38.058594 47.941406 C 38.886719 42.789062 42.910156 38.746094 48.054688 37.886719 L 48.054688 42.179688 L 51.871094 42.179688 L 51.871094 37.847656 C 57.078125 38.648438 61.179688 42.71875 62.019531 47.921875 L 57.707031 47.921875 L 57.707031 51.734375 Z M 62.019531 51.734375 "/>`;
+const collapseIcon = `<svg viewBox="0 0 100 100" width="8" height="8" class="right-triangle"><path fill="currentColor" stroke="currentColor" d="M94.9,20.8c-1.4-2.5-4.1-4.1-7.1-4.1H12.2c-3,0-5.7,1.6-7.1,4.1c-1.3,2.4-1.2,5.2,0.2,7.6L43.1,88c1.5,2.3,4,3.7,6.9,3.7 s5.4-1.4,6.9-3.7l37.8-59.6C96.1,26,96.2,23.2,94.9,20.8L94.9,20.8z"></path></svg>`;
