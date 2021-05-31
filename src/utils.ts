52e307da759ce9c88404a60bbfb15a1bf627bdb5
Stephen Mwangi <st3v3n.mw@gmail.com>
2021-05-31 10:23:45 +0300

    Fix dollar signs replacement

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,9 @@
 // https://stackoverflow.com/a/6969486/12938797
-export function escapeRegexString(text: string) {
+export function escapeRegexString(text: string): string {
     return text.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+// https://stackoverflow.com/questions/38866071/javascript-replace-method-dollar-signs
+export function fixDollarSigns(text: string): string {
+    return text.split("$$").join("$$$");
 }
