8283f9aec79f32eb9aef7d76dfd9826692a48b8b
Nick Olinger <olingern@gmail.com>
2022-03-04 19:35:52 -0500

    format

--- jest.config.js
+++ jest.config.js
@@ -5,11 +5,14 @@ module.exports = {
     moduleNameMapper: {
         "src/(.*)": "<rootDir>/src/$1",
     },
-    collectCoverageFrom: [
-        "src/**/*.ts*"
-    ],
+    collectCoverageFrom: ["src/**/*.ts*"],
     moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node", "d.ts"],
     roots: ["<rootDir>/src/", "<rootDir>/tests/"],
-coveragePathIgnorePatterns: ["/node_modules/", "src/lang/locale/", "src/constants", "src/icons"],
-coverageDirectory: "coverage",
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "src/lang/locale/",
+        "src/constants",
+        "src/icons",
+    ],
+    coverageDirectory: "coverage",
 };
