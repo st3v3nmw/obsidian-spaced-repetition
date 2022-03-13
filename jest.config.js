17440881b58489c04ce8ec90cb66eee01d0839fc
Stephen Mwangi <st3v3n.mw@gmail.com>
2022-03-13 13:22:59 +0300

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -5,8 +5,15 @@ module.exports = {
     moduleNameMapper: {
         "src/(.*)": "<rootDir>/src/$1",
     },
+    collectCoverageFrom: ["src/**/*.ts*"],
     moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node", "d.ts"],
     roots: ["<rootDir>/src/", "<rootDir>/tests/"],
-    coveragePathIgnorePatterns: ["/node_modules/", "src/lang/locale/"],
+    coveragePathIgnorePatterns: [
+        "/node_modules/",
+        "src/lang/locale/",
+        "src/constants",
+        "src/icons",
+        "src/declarations.d.ts",
+    ],
     coverageDirectory: "coverage",
 };
