7390905f5f6a92d8506f704bb396cbfec15da341
Chenyu ZHANG <48207708+zcysxy@users.noreply.github.com>
2022-03-13 15:39:35 +0800

    fix: handle code fences better (#402)
    support more general code fence syntax

--- src/parser.ts
+++ src/parser.ts
@@ -74,12 +74,13 @@ export function parse(
         } else if (lines[i] === multilineReversedCardSeparator) {
             cardType = CardType.MultiLineReversed;
             lineNo = i;
-        } else if (lines[i].startsWith("```")) {
-            while (i + 1 < lines.length && !lines[i + 1].startsWith("```")) {
+        } else if (lines[i].startsWith("```") | lines[i].startsWith("~~~")) {
+            let codeBlockClose = lines[i].match(/`+|~+/);
+            while (i + 1 < lines.length && !lines[i + 1].startsWith(codeBlockClose)) {
                 i++;
                 cardText += "\n" + lines[i];
             }
-            cardText += "\n" + "```";
+            cardText += "\n" + codeBlockClose;
             i++;
         }
     }
