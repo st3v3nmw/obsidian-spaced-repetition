ea162936aa188084d5cbe3d8dab76bce85eb1aef
Lenny Critchley <LennyPhoenixC@gmail.com>
2022-12-13 08:44:43 +0000

    Run prettier

--- src/lang/locale/pt-br.ts
+++ src/lang/locale/pt-br.ts
@@ -28,7 +28,8 @@ export default {
     CRAM_CARDS_IN_NOTE: "Revisar todas as flashcards nessa nota",
     REVIEW_ALL_CARDS: "Revisar flashcards de todas as notas",
     VIEW_STATS: "Ver estatísticas",
-    STATUS_BAR: "Revisão: ${dueNotesCount} nota(s), ${dueFlashcardsCount} Carta(s) para colocar em dia",
+    STATUS_BAR:
+        "Revisão: ${dueNotesCount} nota(s), ${dueFlashcardsCount} Carta(s) para colocar em dia",
     SYNC_TIME_TAKEN: "Sicronização levou ${t}ms",
     NOTE_IN_IGNORED_FOLDER: "Nota é salva na pasta ignorada (cheque as configurações).",
     PLEASE_TAG_NOTE: "Por favor etiquete a nota apropriadamente para revisar (nas configurações).",
@@ -48,7 +49,8 @@ export default {
     SETTINGS_HEADER: "Plguin Spaced Repetition - Configuração",
     CHECK_WIKI: 'Para mais informações, checke o <a href="${wiki_url}">wiki</a>.',
     FOLDERS_TO_IGNORE: "Pastas para ignorar",
-    FOLDERS_TO_IGNORE_DESC: "Ensira o caminho das pastas separado por quebras de linha ex: Templates Meta/Scripts",
+    FOLDERS_TO_IGNORE_DESC:
+        "Ensira o caminho das pastas separado por quebras de linha ex: Templates Meta/Scripts",
     FLASHCARDS: "Flashcards",
     FLASHCARD_EASY_LABEL: "Texto do Botão de Fácil",
     FLASHCARD_GOOD_LABEL: "Texto do Botão de OK",
@@ -60,7 +62,8 @@ export default {
     FLASHCARD_TAGS_DESC:
         "Ensira etiquetas separadas por espaços ou quebras de linha ex: #flashcards #baralho2 #baralho3.",
     CONVERT_FOLDERS_TO_DECKS: "Converter pastas para baralhos e sub-baralhos?",
-    CONVERT_FOLDERS_TO_DECKS_DESC: "Isso é uma alternativa para a opção de etiqueta dos Flashcards em cima.",
+    CONVERT_FOLDERS_TO_DECKS_DESC:
+        "Isso é uma alternativa para a opção de etiqueta dos Flashcards em cima.",
     INLINE_SCHEDULING_COMMENTS:
         "Salvar comentários de agendamento na mesma linha que a última linha do flashcard?",
     INLINE_SCHEDULING_COMMENTS_DESC:
@@ -75,7 +78,8 @@ export default {
         "Deveria estar configurado em 100% em dispositivos móveis ou se você tem imagens muito grandes",
     RESET_DEFAULT: "Reiniciar para a pré-definição",
     CARD_MODAL_WIDTH_PERCENT: "Porcentagem de Largura do Flashcard",
-    FILENAME_OR_OPEN_FILE: "Mostrar o nome do arquivo ao invés de 'Editar Depois' na revisão dos flashcards?",
+    FILENAME_OR_OPEN_FILE:
+        "Mostrar o nome do arquivo ao invés de 'Editar Depois' na revisão dos flashcards?",
     RANDOMIZE_CARD_ORDER: "Aleatorizar a ordem das cartas durante a revisão?",
     DISABLE_CLOZE_CARDS: "Desabilitar cartas que usam omissão de palavras?",
     CONVERT_HIGHLIGHTS_TO_CLOZES: "Converter ==marca-texto== em omissões?",
@@ -89,9 +93,11 @@ export default {
     MULTILINE_REVERSED_CARDS_SEPARATOR: "Separador para flashcards de múltiplas linhas reversos",
     NOTES: "Notas",
     TAGS_TO_REVIEW: "Etiquetas para revisar",
-    TAGS_TO_REVIEW_DESC: "Ensira etiquetas separadas por espaços ou quebra de linhas ex: #revisar #etiqueta2 #etiqueta3.",
+    TAGS_TO_REVIEW_DESC:
+        "Ensira etiquetas separadas por espaços ou quebra de linhas ex: #revisar #etiqueta2 #etiqueta3.",
     OPEN_RANDOM_NOTE: "Abrir uma nota aleatória para revisar",
-    OPEN_RANDOM_NOTE_DESC: "Quando você desabilitar isso, as notas vão ser ordenadas por importância (PageRank).",
+    OPEN_RANDOM_NOTE_DESC:
+        "Quando você desabilitar isso, as notas vão ser ordenadas por importância (PageRank).",
     AUTO_NEXT_NOTE: "Abrir a próxima nota automaticamente depois de uma revisão",
     DISABLE_FILE_MENU_REVIEW_OPTIONS:
         "Desabilitar opções de revisão no menu de arquivos ex: Revisão: Fácil OK Difícil",
@@ -101,7 +107,8 @@ export default {
     MIN_ONE_DAY: "O número de dias deve ser pelo menos 1.",
     VALID_NUMBER_WARNING: "Por favor ensira um número válido.",
     UI_PREFERENCES: "Preferências de UI",
-    INITIALLY_EXPAND_SUBDECKS_IN_TREE: "Árvores de baralhos devem inicialmente serem exibidas como expandidas",
+    INITIALLY_EXPAND_SUBDECKS_IN_TREE:
+        "Árvores de baralhos devem inicialmente serem exibidas como expandidas",
     INITIALLY_EXPAND_SUBDECKS_IN_TREE_DESC:
         "Desabilite isso para colapsar baralhos que estão um dentro do outro na mesma carta. Útil se você tem cartas que pertencem a muitos baralhos em um mesmo arquivo.",
     ALGORITHM: "Algorítmo",
@@ -110,14 +117,16 @@ export default {
     BASE_EASE: "Facilidade base",
     BASE_EASE_DESC: "mínimo = 130, preferivelmente aproximadamente 250.",
     BASE_EASE_MIN_WARNING: "A facilidade base deve ser pelo menos 130.",
-    LAPSE_INTERVAL_CHANGE: "Mudança de intervalo quando você revisa um(a) flashcard/nota como difícil",
+    LAPSE_INTERVAL_CHANGE:
+        "Mudança de intervalo quando você revisa um(a) flashcard/nota como difícil",
     LAPSE_INTERVAL_CHANGE_DESC: "novoIntervalo = velhoIntervalo * mudancaIntervalo / 100.",
     EASY_BONUS: "Bônus de Fácil",
     EASY_BONUS_DESC:
         "O bônus de fácil te permite mudar a diferência entre intervalos de responder OK e Fácil em um(a) flashcard/nota (mínimo = 100%).",
     EASY_BONUS_MIN_WARNING: "O bônus de fácil deve ser pelo menos 100.",
     MAX_INTERVAL: "Intervalo Máximo",
-    MAX_INTERVAL_DESC: "Te permite colocar um limite máximo no intervalo (pré-definição = 100 anos).",
+    MAX_INTERVAL_DESC:
+        "Te permite colocar um limite máximo no intervalo (pré-definição = 100 anos).",
     MAX_INTERVAL_MIN_WARNING: "O intervalo máximo deve ser pelo menos 1 dia.",
     MAX_LINK_CONTRIB: "Contribuição Máxima de Links",
     MAX_LINK_CONTRIB_DESC:
