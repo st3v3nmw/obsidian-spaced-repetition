43060d4f01bd14a9d664597b96fe06eae7223e67
deliriouserror94 <97191345+deliriouserror94@users.noreply.github.com>
2022-12-17 20:15:51 +0000

    Prevent regex from matching to last --- in file (#532)

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const SCHEDULING_INFO_REGEX =
-    /^---\n((?:.*\n)*)sr-due: (.+)\nsr-interval: (\d+)\nsr-ease: (\d+)\n((?:.*\n)*)---/;
+    /^---\n((?:.*\n)*)sr-due: (.+)\nsr-interval: (\d+)\nsr-ease: (\d+)\n((?:.*\n)?)---/;
 export const YAML_FRONT_MATTER_REGEX = /^---\n((?:.*\n)*?)---/;
 
 export const MULTI_SCHEDULING_EXTRACTOR = /!([\d-]+),(\d+),(\d+)/gm;
