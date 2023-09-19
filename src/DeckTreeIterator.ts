7140e62c45564a5b4bf80bf7dca75c74c0b46e4a
ronzulu <75528127+ronzulu@users.noreply.github.com>
2023-09-19 22:20:34 +1000

    Something

--- src/DeckTreeIterator.ts
+++ src/DeckTreeIterator.ts
@@ -12,29 +12,21 @@ export interface IDeckTreeIterator {
     nextCard(): boolean;
 }
 
-export class DeckTreeSequentialIterator implements IDeckTreeIterator {
-    deckTree: Deck;
+class SingleDeckIterator {
+    deck: Deck;
     preferredCardListType: CardListType;
 
-    deckArray: Deck[];
-    deckIdx?: number;
     cardIdx?: number;
     cardListType?: CardListType;
 
     get hasCurrentCard(): boolean {
-        return (this.deckIdx != null) && (this.cardIdx != null);
-    }
-
-    get currentDeck(): Deck {
-        if (this.deckIdx == null)
-            return null;
-        return this.deckArray[this.deckIdx];
+        return (this.cardIdx != null);
     }
 
     get currentCard(): Card {
-        if ((this.deckIdx == null) || (this.cardIdx == null))
+        if (this.cardIdx == null)
             return null;
-        return this.deckArray[this.deckIdx].getCard(this.cardIdx, this.cardListType);
+        return this.deck.getCard(this.cardIdx, this.cardListType);
     }
 
     constructor(preferredCardListType: CardListType) {
@@ -42,52 +34,25 @@ export class DeckTreeSequentialIterator implements IDeckTreeIterator {
     }
 
     setDeck(deck: Deck): void {
-        this.deckTree = deck;
-        this.deckArray = deck.toDeckArray();
-        this.setDeckIdx(null);
-    }
-
-    private setDeckIdx(deckIdx?: number): void {
-        this.deckIdx = deckIdx;
+        this.deck = deck;
         this.cardIdx = null;
         this.cardListType = null;
     }
 
     nextCard(): boolean {
-        let result: boolean = false;
-        if (this.deckIdx == null) {
-            this.setDeckIdx(0);
-        }
-        while (this.deckIdx < this.deckArray.length) {
-            if (this.nextCardWithinDeck()) {
-                result = true;
-                break;
-            }
-            this.deckIdx++;
-        }
-        if (!result)
-            this.deckIdx = null;
-        return result;
-    }
-
-    private nextCardWithinDeck(): boolean {
-        if (this.deckIdx == null)
-            throw "No current deck";
-        let deck: Deck = this.currentDeck;
-
         if (this.cardIdx == null) {
             this.cardIdx = -1;
             this.cardListType = this.preferredCardListType;
         }
 
-        let cardList: Card[] = deck.getCardListForCardType(this.cardListType);
+        let cardList: Card[] = this.deck.getCardListForCardType(this.cardListType);
         this.cardIdx++;
         if (this.cardIdx == cardList.length) {
             if (this.cardListType == this.preferredCardListType) {
                 // Try the non-preferred list type
                 this.cardListType = Deck.otherListType(this.cardListType);
                 this.cardIdx = 0;
-                cardList = deck.getCardListForCardType(this.cardListType);
+                cardList = this.deck.getCardListForCardType(this.cardListType);
                 if (this.cardIdx == cardList.length) {
                     this.cardIdx = null;
                     this.cardListType = null;
@@ -100,25 +65,97 @@ export class DeckTreeSequentialIterator implements IDeckTreeIterator {
         return this.cardIdx != null;
     }
 
-    deleteCurrentCard(): boolean {
+    deleteCurrentCard(): void {
         this.ensureCurrentCard();
-        this.currentDeck.deleteCardAtIndex(this.cardIdx, this.cardListType);
+        this.deck.deleteCardAtIndex(this.cardIdx, this.cardListType);
         this.cardIdx--;
-        return this.nextCard();
     }
 
     moveCurrentCardToEndOfList(): void {
         this.ensureCurrentCard();
-        let cardList: Card[] = this.currentDeck.getCardListForCardType(this.cardListType);
+        let cardList: Card[] = this.deck.getCardListForCardType(this.cardListType);
         if (this.cardIdx == cardList.length - 1)
             return;
         let card = this.currentCard;
-        this.currentDeck.deleteCardAtIndex(this.cardIdx, this.cardListType);
-        this.currentDeck.appendCard(TopicPath.emptyPath, card);
+        this.deck.deleteCardAtIndex(this.cardIdx, this.cardListType);
+        this.deck.appendCard(TopicPath.emptyPath, card);
     }
 
     private ensureCurrentCard() {
         if ((this.cardIdx == null) || (this.cardListType == null))
             throw "no current card";
+    }
+
+}
+
+export class DeckTreeSequentialIterator implements IDeckTreeIterator {
+    deckTree: Deck;
+    preferredCardListType: CardListType;
+
+    singleDeckIterator: SingleDeckIterator;
+    deckArray: Deck[];
+    deckIdx?: number;
+
+    get hasCurrentCard(): boolean {
+        return (this.deckIdx != null) && this.singleDeckIterator.hasCurrentCard;
+    }
+
+    get currentDeck(): Deck {
+        if (this.deckIdx == null)
+            return null;
+        return this.deckArray[this.deckIdx];
+    }
+
+    get currentCard(): Card {
+        if ((this.deckIdx == null) || !this.singleDeckIterator.hasCurrentCard)
+            return null;
+        return this.singleDeckIterator.currentCard;
+    }
+
+    constructor(preferredCardListType: CardListType) {
+        this.preferredCardListType = preferredCardListType;
+        this.singleDeckIterator = new SingleDeckIterator(preferredCardListType);
+    }
+
+    setDeck(deck: Deck): void {
+        this.deckTree = deck;
+        this.deckArray = deck.toDeckArray();
+        this.setDeckIdx(null);
+    }
+
+    private setDeckIdx(deckIdx?: number): void {
+        this.deckIdx = deckIdx;
+        if (deckIdx != null)
+            this.singleDeckIterator.setDeck(this.deckArray[deckIdx]);
+    }
+
+    nextCard(): boolean {
+        let result: boolean = false;
+        if (this.deckIdx == null) {
+            this.setDeckIdx(0);
+        }
+        while (this.deckIdx < this.deckArray.length) {
+            if (this.singleDeckIterator.nextCard()) {
+                result = true;
+                break;
+            }
+            this.deckIdx++;
+            if (this.deckIdx < this.deckArray.length) {
+                this.singleDeckIterator.setDeck(this.deckArray[this.deckIdx]);
+            }
+        }
+        if (!result)
+            this.deckIdx = null;
+        return result;
+    }
+
+
+    deleteCurrentCard(): boolean {
+        this.singleDeckIterator.deleteCurrentCard();
+        return this.nextCard();
+    }
+
+    moveCurrentCardToEndOfList(): void {
+        this.singleDeckIterator.moveCurrentCardToEndOfList();
     }
 }
