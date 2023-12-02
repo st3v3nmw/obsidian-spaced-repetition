141f04dbd2e119fb9ed6d560adbef8d9c7d44032
ronzulu <75528127+ronzulu@users.noreply.github.com>
2023-12-02 22:24:28 +1100

    Removed debug

--- src/main.ts
+++ src/main.ts
@@ -721,15 +721,3 @@ export default class SRPlugin extends Plugin {
         }
     }
 }
-
-function hexEncode(str: string): string{
-    var hex, i;
-
-    var result = "";
-    for (i=0; i<str.length; i++) {
-        hex = str.charCodeAt(i).toString(16);
-        result += ("000"+hex).slice(-4) + " ";
-    }
-
-    return result
-}
