54fbf81e932b4c71b6fc58880cf9752c1db61ffa
ronzulu <75528127+ronzulu@users.noreply.github.com>
2024-01-29 00:19:57 +1100

    Added code to access the frontmatter tags

--- src/SRFile.ts
+++ src/SRFile.ts
@@ -1,4 +1,4 @@
-import { MetadataCache, TFile, Vault, HeadingCache, TagCache } from "obsidian";
+import { MetadataCache, TFile, Vault, HeadingCache, TagCache, FrontMatterCache } from "obsidian";
 
 export interface ISRFile {
     get path(): string;
@@ -29,10 +29,42 @@ export class SrTFile implements ISRFile {
     }
 
     getAllTagsFromText(): TagCache[] {
-        let result: TagCache[];
+        let result: TagCache[] = [] as TagCache[];
         const fileCachedData = this.metadataCache.getFileCache(this.file) || {};
-        if (fileCachedData.tags?.length > 0) result = fileCachedData.tags;
-        else result = [] as TagCache[];
+        if (fileCachedData.tags?.length > 0) {
+            result.push(...fileCachedData.tags);
+        }
+
+        // RZ: 2024-01-28 fileCachedData.tags doesn't include the tags within the frontmatter, need to access those separately
+        // This is different to the Obsidian function getAllTags() which does return all tags including those within the
+        // frontmatter
+        result.push(...this.getFrontmatterTags(fileCachedData.frontmatter));
+
+        return result;
+    }
+
+    private getFrontmatterTags(frontmatter: FrontMatterCache): TagCache[] {
+        let result: TagCache[] = [] as TagCache[];
+        const frontmatterTags: string = (frontmatter != null) ? frontmatter["tags"] + '' : null;
+        if (frontmatterTags) {
+            // The frontmatter doesn't include the line number for the specific tag, defining as line 1 is good enough.
+            // (determineQuestionTopicPathList() only needs to know that these frontmatter tags come before all others
+            // in the file)
+            const line: number = 1;
+
+            // Frontmatter tags are comma separated and don't include the "#", so we need to add that in
+            const tagStrList: string[] = frontmatterTags.split(",");
+            for (const str of tagStrList) {
+                const tag: TagCache = {
+                    tag: "#" + str,
+                    position: {
+                        start: { line: line, col: null, offset: null },
+                        end: { line: line, col: null, offset: null },
+                    },
+                };
+                result.push(tag);
+            }
+        }
         return result;
     }
 
