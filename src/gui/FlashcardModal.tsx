d4b8d016ac789216a01a3374b03c2c6ab00fffbf
Kyle Klus <kyle.klus.2@gmail.com>
2024-03-20 19:54:42 +0100

    [Overhaul] Implementation of a more consistent & modern UI (#899)
    * UPDATE: Refactored each gui "view" into a separate file, so it is less cluttered and not a monolithic file
    
    * FIX: Rewrote the keydown handler to make it a bit nicer
    
    * UPDATE: formatted code
    
    * UPDATE: Documented changes
    
    * FIX: Fixed a bug where the keydown handler didn't ignore a key if the back wasn't shown
    
    * UPDATE: Reimplemented the title in the decks list and implemented a way of showing/hiding without the need of rerendering everything
    
    * UPDATE: Updated class naming scheme
    
    * UPDATE: Changed the deck list header style a bit
    
    * UPDATE: cleaned some old code
    
    * UPDATE: Another bit of code cleanup
    
    * FIX: Fixed that the close button wasn't clickable
    
    * UPDATE: Redesigned DecksListView
    
    * UPDATE: Updated the DecksListView design
    
    * UPDATE: Major reimplantation of the FlashcardsReviewView & again small changes to the DecksListView
    
    * UPDATE: Redesigned the edit modal
    
    * UPDATED: Renamed files, such that the naming convention is the same everywhere.
    
    * UPDATE: Code cleanup & added comment
    
    * UPDATE: Added modal styles to the edit modal & commented the styles file
    
    * UPDATE: Documented code changes
    
    * FIX: Fixed that the overflow behavior cause the buttons to move
    
    * UPDATE: formatted with prettier
    
    * FIX: Fixed mobile view
    
    * FIX: Fixed that mobile view wasn't spanning the whole screen, like it used to do
    
    * UPDATE: Formatted with prettier
    
    * UPDATE: Shortened changelog as requested in the review
    
    * FIX: Fixed unused variables warning
    
    * FIX: Title was hidden for some reason
    
    * UPDATE: Used is-mobile class, instead of is-phone
    
    * UPDATE: Improved backbutton position
    
    * UPDATE: Improved context positioning
    
    * FIX: Implemented a better close condition
    
    * FIX: removed debug code
    
    * FIX: Fixed that the keydown handler did respond, even if the window was closed
    
    * UPDATE: Updated changelog & formatted code
    
    * FIX: Fixed parts, as requested in review
    
    * Update changelog.md
    
    * UPDATE: Added issue links to the changelog & formatted files
    
    ---------
    
    Co-authored-by: Stephen Mwangi <mail@stephenmwangi.com>

--- src/gui/FlashcardModal.tsx
+++ src/gui/FlashcardModal.tsx
@@ -4,14 +4,15 @@ import h from "vhtml";
 
 import type SRPlugin from "src/main";
 import { SRSettings } from "src/settings";
+
 import { Deck } from "../Deck";
 import { Question } from "../Question";
 import {
     FlashcardReviewMode,
     IFlashcardReviewSequencer as IFlashcardReviewSequencer,
 } from "src/FlashcardReviewSequencer";
-import { FlashcardEditModal } from "./flashcards-edit-modal";
-import { DecksListView } from "./DecksListView";
+import { FlashcardEditModal } from "./EditModal";
+import { DeckListView } from "./DeckListView";
 import { FlashcardReviewView } from "./FlashcardReviewView";
 
 export enum FlashcardModalMode {
@@ -27,6 +28,8 @@ export class FlashcardModal extends Modal {
     private reviewSequencer: IFlashcardReviewSequencer;
     private settings: SRSettings;
     private reviewMode: FlashcardReviewMode;
+    private deckView: DeckListView;
+    private flashcardView: FlashcardReviewView;
 
     constructor(
         app: App,
@@ -37,55 +40,79 @@ export class FlashcardModal extends Modal {
     ) {
         super(app);
 
+        // Init properties
         this.plugin = plugin;
         this.settings = settings;
         this.reviewSequencer = reviewSequencer;
         this.reviewMode = reviewMode;
 
+        // Setup base containers
         this.modalEl.style.height = this.settings.flashcardHeightPercentage + "%";
         this.modalEl.style.width = this.settings.flashcardWidthPercentage + "%";
-        this.modalEl.addClass("sr-modal");
-    }
+        this.modalEl.setAttribute("id", "sr-modal");
 
-    onOpen(): void {
-        this.showDecksList();
-    }
+        this.contentEl.addClass("sr-modal-content");
 
-    onClose(): void {
-        this.mode = FlashcardModalMode.Closed;
-    }
-
-    showDecksList(): void {
-        new DecksListView(
+        // Init static elements in views
+        this.deckView = new DeckListView(
             this.plugin,
             this.settings,
             this.reviewSequencer,
-            this.titleEl,
             this.contentEl,
-            this.startReviewOfDeck.bind(this),
-        ).show();
+            this._startReviewOfDeck.bind(this),
+        );
+
+        this.flashcardView = new FlashcardReviewView(
+            this.app,
+            this.plugin,
+            this.settings,
+            this.reviewSequencer,
+            this.reviewMode,
+            this.contentEl,
+            this.modalEl,
+            this._showDecksList.bind(this),
+            this._doEditQuestionText.bind(this),
+        );
     }
 
-    startReviewOfDeck(deck: Deck) {
+    onOpen(): void {
+        this._showDecksList();
+    }
+
+    onClose(): void {
+        this.deckView.close();
+        this.flashcardView.close();
+        this.mode = FlashcardModalMode.Closed;
+    }
+
+    private _showDecksList(): void {
+        this._hideFlashcard();
+        this.deckView.show();
+    }
+
+    private _hideDecksList(): void {
+        this.deckView.hide();
+    }
+
+    private _showFlashcard(): void {
+        this._hideDecksList();
+        this.flashcardView.show();
+    }
+
+    private _hideFlashcard(): void {
+        this.flashcardView.hide();
+    }
+
+    private _startReviewOfDeck(deck: Deck) {
         this.reviewSequencer.setCurrentDeck(deck.getTopicPath());
         if (this.reviewSequencer.hasCurrentCard) {
-            new FlashcardReviewView(
-                this.app,
-                this.plugin,
-                this.settings,
-                this.reviewSequencer,
-                this.reviewMode,
-                this.titleEl,
-                this.contentEl,
-                this.showDecksList.bind(this),
-                this.doEditQuestionText.bind(this),
-            ).showCurrentCard();
+            this._showFlashcard();
         } else {
-            this.showDecksList();
+            this._showDecksList();
         }
     }
 
-    async doEditQuestionText(): Promise<void> {
+    private async _doEditQuestionText(): Promise<void> {
         const currentQ: Question = this.reviewSequencer.currentQuestion;
 
         // Just the question/answer text; without any preceding topic tag
