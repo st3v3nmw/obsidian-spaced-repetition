67bd96d2b18d734959dffdb92c39601decf9e854
KyleKlus <kyle.klus.2@gmail.com>
2024-05-10 13:40:18 +0200

    UPDATE: Extended the amount of information in the card title

--- src/gui/FlashcardModal.tsx
+++ src/gui/FlashcardModal.tsx
@@ -80,9 +80,9 @@ export class FlashcardModal extends Modal {
     }
 
     onClose(): void {
+        this.mode = FlashcardModalMode.Closed;
         this.deckView.close();
         this.flashcardView.close();
-        this.mode = FlashcardModalMode.Closed;
     }
 
     private _showDecksList(): void {
@@ -94,9 +94,9 @@ export class FlashcardModal extends Modal {
         this.deckView.hide();
     }
 
-    private _showFlashcard(): void {
+    private _showFlashcard(deck:Deck): void {
         this._hideDecksList();
-        this.flashcardView.show();
+        this.flashcardView.show(deck);
     }
 
     private _hideFlashcard(): void {
@@ -106,7 +106,7 @@ export class FlashcardModal extends Modal {
     private _startReviewOfDeck(deck: Deck) {
         this.reviewSequencer.setCurrentDeck(deck.getTopicPath());
         if (this.reviewSequencer.hasCurrentCard) {
-            this._showFlashcard();
+            this._showFlashcard(deck);
         } else {
             this._showDecksList();
         }
