8ceb3da396ba21b2d4f5925a3a5cfc6e6e36b06d
ronzulu <75528127+ronzulu@users.noreply.github.com>
2024-07-03 17:52:54 +1000

    Update

--- src/main.ts
+++ src/main.ts
@@ -92,7 +92,7 @@ export default class SRPlugin extends Plugin {
     public cardStats: Stats;
 
     async onload(): Promise<void> {
-        console.log("onload: Branch: master v1.13-beta.4");
+        console.log("onload: Branch: master v1.13-beta.5");
         await this.loadPluginData();
         this.easeByPath = new NoteEaseList(this.data.settings);
         this.questionPostponementList = new QuestionPostponementList(
