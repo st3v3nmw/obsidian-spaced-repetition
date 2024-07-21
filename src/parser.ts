efa5917b00bb2a4e3f0348428f8316ce61482d03
Andrea Alberti <a.alberti82@gmail.com>
2024-07-21 19:53:54 +0200

    Testing the parser

--- src/parser.ts
+++ src/parser.ts
@@ -40,7 +40,7 @@ export class ParsedQuestionInfo {
  * @param multilineCardEndMarker - Marker indicating the end of a multi-line card
  * @returns An array of [CardType, card text, line number] tuples
  */
-export function parseExNew(
+export function parseEx(
 	text: string,
 	singlelineCardSeparator: string,
 	singlelineReversedCardSeparator: string,
@@ -77,6 +77,142 @@ export function parseExNew(
 	return cards;
 }
 
+// /**
+//  * Returns flashcards found in `text`
+//  *
+//  * It is best that the text does not contain frontmatter, see extractFrontmatter for reasoning
+//  *
+//  * Multi-line question with blank lines user workaround:
+//  *      As of 3/04/2024 there is no support for including blank lines within multi-line questions
+//  *      As a workaround, one user uses a zero width Unicode character - U+200B
+//  *      https://github.com/st3v3nmw/obsidian-spaced-repetition/issues/915#issuecomment-2031003092
+//  *
+//  * @param text - The text to extract flashcards from
+//  * @param singlelineCardSeparator - Separator for inline basic cards
+//  * @param singlelineReversedCardSeparator - Separator for inline reversed cards
+//  * @param multilineCardSeparator - Separator for multiline basic cards
+//  * @param multilineReversedCardSeparator - Separator for multiline basic card
+//  * @param multilineCardEndMarker - Marker indicating the end of a multi-line card
+//  * @returns An array of [CardType, card text, line number] tuples
+//  */
+// export function parseExOld(
+// 	text: string,
+// 	singlelineCardSeparator: string,
+// 	singlelineReversedCardSeparator: string,
+// 	multilineCardSeparator: string,
+// 	multilineReversedCardSeparator: string,
+// 	multilineCardEndMarker: string,
+// 	convertHighlightsToClozes: boolean,
+// 	convertBoldTextToClozes: boolean,
+// 	convertCurlyBracketsToClozes: boolean,
+// ): ParsedQuestionInfo[] {
+// 	let cardText = "";
+// 	const cards: ParsedQuestionInfo[] = [];
+// 	let cardType: CardType | null = null;
+// 	let firstLineNo = 0;
+// 	let lastLineNo = 0;
+// 	let annotation = "";
+
+// 	const lines: string[] = text.replaceAll("\r\n", "\n").split("\n");
+// 	for (let i = 0; i < lines.length; i++) {
+// 		const currentLine = lines[i];
+
+// 		// Ignore non-SR comments
+// 		if (currentLine.startsWith("<!--") && !currentLine.startsWith("<!--SR:")) {
+// 			while (i + 1 < lines.length && !lines[i + 1].includes("-->")) i++;
+// 			i++;
+// 			continue;
+// 		}
+
+// 		// Capture SR annotations
+// 		if (currentLine.startsWith("<!--SR:!")) {
+// 			annotation = currentLine;
+// 			lastLineNo = i;
+// 			continue;
+// 		}
+
+// 		// Check for end marker for multi-line cards
+// 		if ( cardType && currentLine.trim() === multilineCardEndMarker ) {
+// 			if (annotation) {
+// 				cardText = cardText.trimEnd() + "\n" + annotation;
+// 				annotation = "";
+// 			}
+// 			cards.push(new ParsedQuestionInfo(cardType, cardText.trimEnd(), firstLineNo, lastLineNo-1));
+// 			cardType = null;
+// 			cardText = "";
+// 			continue;
+// 		}
+
+// 		if(currentLine.length > 0) {
+// 			lastLineNo = i;
+// 		}
+
+// 		// Concatenate lines for the current card
+// 		if (cardText.length > 0) {
+// 			cardText += "\n";
+// 		} else {
+// 			firstLineNo = i;
+// 		}
+// 		cardText += currentLine;
+
+// 		// Handle single-line cards
+// 		if (
+// 			currentLine.includes(singlelineReversedCardSeparator) ||
+// 			currentLine.includes(singlelineCardSeparator)
+// 		) {
+// 			cardType = currentLine.includes(singlelineReversedCardSeparator)
+// 				? CardType.SingleLineReversed
+// 				: CardType.SingleLineBasic;
+// 			cardText = currentLine;
+// 			firstLineNo = i;
+// 			if (i + 1 < lines.length && lines[i + 1].startsWith("<!--SR:")) {
+// 				cardText += "\n" + lines[i + 1];
+// 				i++;
+// 			}
+// 			const lastLineNo = i;
+// 			cards.push(new ParsedQuestionInfo(cardType, cardText.trimEnd(), firstLineNo, lastLineNo));
+// 			cardType = null;
+// 			cardText = "";
+// 		} else if (cardType === null && currentLine.trim() === multilineCardSeparator) {
+// 			cardType = CardType.MultiLineBasic;
+// 			// Explicitly don't change firstLineNo, as per above comment
+// 		} else if (cardType === null && currentLine.trim() === multilineReversedCardSeparator) {
+// 			cardType = CardType.MultiLineReversed;
+// 			// Explicitly don't change firstLineNo, as per above comment
+// 		} else if (
+// 			cardType === null &&
+// 			((convertHighlightsToClozes && /==.*?==/gm.test(currentLine)) ||
+// 				(convertBoldTextToClozes && /\*\*.*?\*\*/gm.test(currentLine)) ||
+// 				(convertCurlyBracketsToClozes && /{{.*?}}/gm.test(currentLine)))
+// 		) {
+// 			cardType = CardType.Cloze;
+
+// 			// Explicitly don't change firstLineNo, as we might not see the cloze markers on the first line
+// 			// of a multi line cloze question. I.e. firstLineNo may be less than i;
+// 		} else if (currentLine.startsWith("```") || currentLine.startsWith("~~~")) {
+// 			const codeBlockClose = currentLine.match(/`+|~+/)[0];
+// 			while (i + 1 < lines.length && !lines[i + 1].startsWith(codeBlockClose)) {
+// 				i++;
+// 				cardText += "\n" + lines[i];
+// 			}
+// 			cardText += "\n" + codeBlockClose;
+// 			i++;
+// 		}
+// 	}
+
+// 	// Handle the last card if any
+// 	if (cardType && cardText) {
+// 		const lastLineNo = lines.length - 1;
+// 		if (annotation) {
+// 			cardText += "\n" + annotation;
+// 		}
+// 		cards.push(new ParsedQuestionInfo(cardType, cardText.trimEnd(), firstLineNo, lastLineNo));
+// 	}
+// 	console.log(`OLD PARSER: PROCESSED ${cards.length} FLASHCARDS!`);
+// 	console.log(cards);
+// 	return cards;
+// }
+
 /**
  * Returns flashcards found in `text`
  *
@@ -92,235 +228,99 @@ export function parseExNew(
  * @param singlelineReversedCardSeparator - Separator for inline reversed cards
  * @param multilineCardSeparator - Separator for multiline basic cards
  * @param multilineReversedCardSeparator - Separator for multiline basic card
- * @param multilineCardEndMarker - Marker indicating the end of a multi-line card
  * @returns An array of [CardType, card text, line number] tuples
  */
-export function parseExOld(
-	text: string,
-	singlelineCardSeparator: string,
-	singlelineReversedCardSeparator: string,
-	multilineCardSeparator: string,
-	multilineReversedCardSeparator: string,
-	multilineCardEndMarker: string,
-	convertHighlightsToClozes: boolean,
-	convertBoldTextToClozes: boolean,
-	convertCurlyBracketsToClozes: boolean,
-): ParsedQuestionInfo[] {
-	let cardText = "";
-	const cards: ParsedQuestionInfo[] = [];
-	let cardType: CardType | null = null;
-	let firstLineNo = 0;
-	let lastLineNo = 0;
-	let annotation = "";
+// export function parseExOld(
+//     text: string,
+//     singlelineCardSeparator: string,
+//     singlelineReversedCardSeparator: string,
+//     multilineCardSeparator: string,
+//     multilineReversedCardSeparator: string,
+//     multilineCardEndMarker: string,
+//     convertHighlightsToClozes: boolean,
+//     convertBoldTextToClozes: boolean,
+//     convertCurlyBracketsToClozes: boolean,
+// ): ParsedQuestionInfo[] {
+//     let cardText = "";
+//     const cards: ParsedQuestionInfo[] = [];
+//     let cardType: CardType | null = null;
+//     let firstLineNo = 0;
+//     let lastLineNo = 0;
 
-	const lines: string[] = text.replaceAll("\r\n", "\n").split("\n");
-	for (let i = 0; i < lines.length; i++) {
-		const currentLine = lines[i];
+//     const lines: string[] = text.replaceAll("\r\n", "\n").split("\n");
+//     for (let i = 0; i < lines.length; i++) {
+//         const currentLine = lines[i];
+//         if (currentLine.length === 0) {
+//             if (cardType) {
+//                 lastLineNo = i - 1;
+//                 cards.push(new ParsedQuestionInfo(cardType, cardText, firstLineNo, lastLineNo));
+//                 cardType = null;
+//             }
 
-		// Ignore non-SR comments
-		if (currentLine.startsWith("<!--") && !currentLine.startsWith("<!--SR:")) {
-			while (i + 1 < lines.length && !lines[i + 1].includes("-->")) i++;
-			i++;
-			continue;
-		}
+//             cardText = "";
+//             continue;
+//         } else if (currentLine.startsWith("<!--") && !currentLine.startsWith("<!--SR:")) {
+//             while (i + 1 < lines.length && !currentLine.includes("-->")) i++;
+//             i++;
+//             continue;
+//         }
 
-		// Capture SR annotations
-		if (currentLine.startsWith("<!--SR:!")) {
-			annotation = currentLine;
-			lastLineNo = i;
-			continue;
-		}
+//         if (cardText.length > 0) {
+//             cardText += "\n";
+//         } else if (cardText.length === 0) {
+//             // This could be the first line of a multi line question
+//             firstLineNo = i;
+//         }
+//         cardText += currentLine.trimEnd();
 
-		// Check for end marker for multi-line cards
-		if ( cardType && currentLine.trim() === multilineCardEndMarker ) {
-			if (annotation) {
-				cardText = cardText.trimEnd() + "\n" + annotation;
-				annotation = "";
-			}
-			cards.push(new ParsedQuestionInfo(cardType, cardText.trimEnd(), firstLineNo, lastLineNo-1));
-			cardType = null;
-			cardText = "";
-			continue;
-		}
+//         if (
+//             currentLine.includes(singlelineReversedCardSeparator) ||
+//             currentLine.includes(singlelineCardSeparator)
+//         ) {
+//             cardType = lines[i].includes(singlelineReversedCardSeparator)
+//                 ? CardType.SingleLineReversed
+//                 : CardType.SingleLineBasic;
+//             cardText = lines[i];
+//             firstLineNo = i;
+//             if (i + 1 < lines.length && lines[i + 1].startsWith("<!--SR:")) {
+//                 cardText += "\n" + lines[i + 1];
+//                 i++;
+//             }
+//             lastLineNo = i;
+//             cards.push(new ParsedQuestionInfo(cardType, cardText, firstLineNo, lastLineNo));
+//             cardType = null;
+//             cardText = "";
+//         } else if (
+//             cardType === null &&
+//             ((convertHighlightsToClozes && /==.*?==/gm.test(currentLine)) ||
+//                 (convertBoldTextToClozes && /\*\*.*?\*\*/gm.test(currentLine)) ||
+//                 (convertCurlyBracketsToClozes && /{{.*?}}/gm.test(currentLine)))
+//         ) {
+//             cardType = CardType.Cloze;
 
-		if(currentLine.length > 0) {
-			lastLineNo = i;
-		}
+//             // Explicitly don't change firstLineNo, as we might not see the cloze markers on the first line
+//             // of a multi line cloze question. I.e. firstLineNo may be less than i;
+//         } else if (currentLine.trim() === multilineCardSeparator) {
+//             cardType = CardType.MultiLineBasic;
+//             // Explicitly don't change firstLineNo, as per above comment
+//         } else if (currentLine.trim() === multilineReversedCardSeparator) {
+//             cardType = CardType.MultiLineReversed;
+//             // Explicitly don't change firstLineNo, as per above comment
+//         } else if (currentLine.startsWith("```") || currentLine.startsWith("~~~")) {
+//             const codeBlockClose = currentLine.match(/`+|~+/)[0];
+//             while (i + 1 < lines.length && !lines[i + 1].startsWith(codeBlockClose)) {
+//                 i++;
+//                 cardText += "\n" + lines[i];
+//             }
+//             cardText += "\n" + codeBlockClose;
+//             i++;
+//         }
+//     }
 
-		// Concatenate lines for the current card
-		if (cardText.length > 0) {
-			cardText += "\n";
-		} else {
-			firstLineNo = i;
-		}
-		cardText += currentLine;
+//     if (cardType && cardText) {
+//         lastLineNo = lines.length - 1;
+//         cards.push(new ParsedQuestionInfo(cardType, cardText, firstLineNo, lastLineNo));
+//     }
 
-		// Handle single-line cards
-		if (
-			currentLine.includes(singlelineReversedCardSeparator) ||
-			currentLine.includes(singlelineCardSeparator)
-		) {
-			cardType = currentLine.includes(singlelineReversedCardSeparator)
-				? CardType.SingleLineReversed
-				: CardType.SingleLineBasic;
-			cardText = currentLine;
-			firstLineNo = i;
-			if (i + 1 < lines.length && lines[i + 1].startsWith("<!--SR:")) {
-				cardText += "\n" + lines[i + 1];
-				i++;
-			}
-			const lastLineNo = i;
-			cards.push(new ParsedQuestionInfo(cardType, cardText.trimEnd(), firstLineNo, lastLineNo));
-			cardType = null;
-			cardText = "";
-		} else if (cardType === null && currentLine.trim() === multilineCardSeparator) {
-			cardType = CardType.MultiLineBasic;
-			// Explicitly don't change firstLineNo, as per above comment
-		} else if (cardType === null && currentLine.trim() === multilineReversedCardSeparator) {
-			cardType = CardType.MultiLineReversed;
-			// Explicitly don't change firstLineNo, as per above comment
-		} else if (
-			cardType === null &&
-			((convertHighlightsToClozes && /==.*?==/gm.test(currentLine)) ||
-				(convertBoldTextToClozes && /\*\*.*?\*\*/gm.test(currentLine)) ||
-				(convertCurlyBracketsToClozes && /{{.*?}}/gm.test(currentLine)))
-		) {
-			cardType = CardType.Cloze;
-
-			// Explicitly don't change firstLineNo, as we might not see the cloze markers on the first line
-			// of a multi line cloze question. I.e. firstLineNo may be less than i;
-		} else if (currentLine.startsWith("```") || currentLine.startsWith("~~~")) {
-			const codeBlockClose = currentLine.match(/`+|~+/)[0];
-			while (i + 1 < lines.length && !lines[i + 1].startsWith(codeBlockClose)) {
-				i++;
-				cardText += "\n" + lines[i];
-			}
-			cardText += "\n" + codeBlockClose;
-			i++;
-		}
-	}
-
-	// Handle the last card if any
-	if (cardType && cardText) {
-		const lastLineNo = lines.length - 1;
-		if (annotation) {
-			cardText += "\n" + annotation;
-		}
-		cards.push(new ParsedQuestionInfo(cardType, cardText.trimEnd(), firstLineNo, lastLineNo));
-	}
-	console.log(`OLD PARSER: PROCESSED ${cards.length} FLASHCARDS!`);
-	console.log(cards);
-	return cards;
-}
-
-/**
- * Returns flashcards found in `text`
- *
- * It is best that the text does not contain frontmatter, see extractFrontmatter for reasoning
- *
- * Multi-line question with blank lines user workaround:
- *      As of 3/04/2024 there is no support for including blank lines within multi-line questions
- *      As a workaround, one user uses a zero width Unicode character - U+200B
- *      https://github.com/st3v3nmw/obsidian-spaced-repetition/issues/915#issuecomment-2031003092
- *
- * @param text - The text to extract flashcards from
- * @param singlelineCardSeparator - Separator for inline basic cards
- * @param singlelineReversedCardSeparator - Separator for inline reversed cards
- * @param multilineCardSeparator - Separator for multiline basic cards
- * @param multilineReversedCardSeparator - Separator for multiline basic card
- * @returns An array of [CardType, card text, line number] tuples
- */
-export function parseEx(
-    text: string,
-    singlelineCardSeparator: string,
-    singlelineReversedCardSeparator: string,
-    multilineCardSeparator: string,
-    multilineReversedCardSeparator: string,
-    multilineCardEndMarker: string,
-    convertHighlightsToClozes: boolean,
-    convertBoldTextToClozes: boolean,
-    convertCurlyBracketsToClozes: boolean,
-): ParsedQuestionInfo[] {
-    let cardText = "";
-    const cards: ParsedQuestionInfo[] = [];
-    let cardType: CardType | null = null;
-    let firstLineNo = 0;
-    let lastLineNo = 0;
-
-    const lines: string[] = text.replaceAll("\r\n", "\n").split("\n");
-    for (let i = 0; i < lines.length; i++) {
-        const currentLine = lines[i];
-        if (currentLine.length === 0) {
-            if (cardType) {
-                lastLineNo = i - 1;
-                cards.push(new ParsedQuestionInfo(cardType, cardText, firstLineNo, lastLineNo));
-                cardType = null;
-            }
-
-            cardText = "";
-            continue;
-        } else if (currentLine.startsWith("<!--") && !currentLine.startsWith("<!--SR:")) {
-            while (i + 1 < lines.length && !currentLine.includes("-->")) i++;
-            i++;
-            continue;
-        }
-
-        if (cardText.length > 0) {
-            cardText += "\n";
-        } else if (cardText.length === 0) {
-            // This could be the first line of a multi line question
-            firstLineNo = i;
-        }
-        cardText += currentLine.trimEnd();
-
-        if (
-            currentLine.includes(singlelineReversedCardSeparator) ||
-            currentLine.includes(singlelineCardSeparator)
-        ) {
-            cardType = lines[i].includes(singlelineReversedCardSeparator)
-                ? CardType.SingleLineReversed
-                : CardType.SingleLineBasic;
-            cardText = lines[i];
-            firstLineNo = i;
-            if (i + 1 < lines.length && lines[i + 1].startsWith("<!--SR:")) {
-                cardText += "\n" + lines[i + 1];
-                i++;
-            }
-            lastLineNo = i;
-            cards.push(new ParsedQuestionInfo(cardType, cardText, firstLineNo, lastLineNo));
-            cardType = null;
-            cardText = "";
-        } else if (
-            cardType === null &&
-            ((convertHighlightsToClozes && /==.*?==/gm.test(currentLine)) ||
-                (convertBoldTextToClozes && /\*\*.*?\*\*/gm.test(currentLine)) ||
-                (convertCurlyBracketsToClozes && /{{.*?}}/gm.test(currentLine)))
-        ) {
-            cardType = CardType.Cloze;
-
-            // Explicitly don't change firstLineNo, as we might not see the cloze markers on the first line
-            // of a multi line cloze question. I.e. firstLineNo may be less than i;
-        } else if (currentLine.trim() === multilineCardSeparator) {
-            cardType = CardType.MultiLineBasic;
-            // Explicitly don't change firstLineNo, as per above comment
-        } else if (currentLine.trim() === multilineReversedCardSeparator) {
-            cardType = CardType.MultiLineReversed;
-            // Explicitly don't change firstLineNo, as per above comment
-        } else if (currentLine.startsWith("```") || currentLine.startsWith("~~~")) {
-            const codeBlockClose = currentLine.match(/`+|~+/)[0];
-            while (i + 1 < lines.length && !lines[i + 1].startsWith(codeBlockClose)) {
-                i++;
-                cardText += "\n" + lines[i];
-            }
-            cardText += "\n" + codeBlockClose;
-            i++;
-        }
-    }
-
-    if (cardType && cardText) {
-        lastLineNo = lines.length - 1;
-        cards.push(new ParsedQuestionInfo(cardType, cardText, firstLineNo, lastLineNo));
-    }
-
-    return cards;
-}
+//     return cards;
+// }
