3228e9ccaa44a8969f722e8c7c445c2dd820966f
Ronny Zulaikha <75528127+ronzulu@users.noreply.github.com>
2024-07-24 15:10:39 +1000

    Support RTL flashcards specified by frontmatter "direction" attribute (#935)
    * Nearly completed
    
    * Added RTL support for flashcards edit modal
    
    * Changes as part of the merge
    
    * post upstream master merge fixes
    
    * Minor code improvement
    
    * lint and format
    
    * Change log and documentation update
    
    * Minor code change
    
    * Fixed EditModal RTL
    
    * lint and format
    
    * Updated test cases to fix global coverage error
    
    * Format & lint

--- src/gui/FlashcardReviewView.tsx
+++ src/gui/FlashcardReviewView.tsx
@@ -136,7 +136,11 @@ export class FlashcardReviewView {
             this.plugin,
             this._currentNote.filePath,
         );
-        await wrapper.renderMarkdownWrapper(this._currentCard.front, this.content);
+        await wrapper.renderMarkdownWrapper(
+            this._currentCard.front,
+            this.content,
+            this._currentQuestion.questionText.textDirection,
+        );
         // Set scroll position back to top
         this.content.scrollTop = 0;
 
@@ -292,7 +296,11 @@ export class FlashcardReviewView {
             this.plugin,
             this._currentNote.filePath,
         );
-        wrapper.renderMarkdownWrapper(this._currentCard.back, this.content);
+        wrapper.renderMarkdownWrapper(
+            this._currentCard.back,
+            this.content,
+            this._currentQuestion.questionText.textDirection,
+        );
 
         // Show response buttons
         this.answerButton.addClass("sr-is-hidden");
