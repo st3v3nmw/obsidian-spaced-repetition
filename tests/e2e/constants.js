2f3a597bedd3789da0fc5c8feb31977fcb0a7978
Stephen Mwangi <mail@stephenmwangi.com>
2024-09-21 19:32:16 +0300

    chore: remove e2e testing
    Never got E2E testing to work, remove it. It's laborious to keep
    up to date anyways.

--- tests/e2e/constants.js
+++
@@ -1,6 +0,0 @@
-// TODO: Fix e2e tests
-
-// module.exports = {
-//     SCHEDULING_INFO_REGEX:
-//         /^---\n((?:.*\n)*)sr-due: (.+)\nsr-interval: (\d+)\nsr-ease: (\d+)\n((?:.*\n)?)---/,
-// };
