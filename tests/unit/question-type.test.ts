0e7e2fd9c703a2e04c5f9541319797bae60f7a74
currently-coding <165482223+currently-coding@users.noreply.github.com>
2025-06-28 10:05:43 +0200

    added question-type test

--- tests/unit/question-type.test.ts
+++ tests/unit/question-type.test.ts
@@ -3,98 +3,136 @@ import { CardFrontBack, CardFrontBackUtil, QuestionTypeClozeFormatter } from "sr
 import { DEFAULT_SETTINGS, SRSettings } from "src/settings";
 
 test("CardType.SingleLineBasic", () => {
-    expect(CardFrontBackUtil.expand(CardType.SingleLineBasic, "A::B", DEFAULT_SETTINGS)).toEqual([
-        new CardFrontBack("A", "B"),
-    ]);
+	expect(CardFrontBackUtil.expand(CardType.SingleLineBasic, "A::B", DEFAULT_SETTINGS)).toEqual([
+		new CardFrontBack("A", "B"),
+	]);
 });
 
 test("CardType.SingleLineReversed", () => {
-    expect(
-        CardFrontBackUtil.expand(CardType.SingleLineReversed, "A:::B", DEFAULT_SETTINGS),
-    ).toEqual([new CardFrontBack("A", "B"), new CardFrontBack("B", "A")]);
+	expect(
+		CardFrontBackUtil.expand(CardType.SingleLineReversed, "A:::B", DEFAULT_SETTINGS),
+	).toEqual([new CardFrontBack("A", "B"), new CardFrontBack("B", "A")]);
 });
 
 describe("CardType.MultiLineBasic", () => {
-    test("Basic", () => {
-        expect(
-            CardFrontBackUtil.expand(
-                CardType.MultiLineBasic,
-                "A1\nA2\n?\nB1\nB2",
-                DEFAULT_SETTINGS,
-            ),
-        ).toEqual([new CardFrontBack("A1\nA2", "B1\nB2")]);
-    });
+	test("Basic", () => {
+		expect(
+			CardFrontBackUtil.expand(
+				CardType.MultiLineBasic,
+				"A1\nA2\n?\nB1\nB2",
+				DEFAULT_SETTINGS,
+			),
+		).toEqual([new CardFrontBack("A1\nA2", "B1\nB2")]);
+	});
 });
 
 test("CardType.MultiLineReversed", () => {
-    expect(
-        CardFrontBackUtil.expand(
-            CardType.MultiLineReversed,
-            "A1\nA2\n??\nB1\nB2",
-            DEFAULT_SETTINGS,
-        ),
-    ).toEqual([new CardFrontBack("A1\nA2", "B1\nB2"), new CardFrontBack("B1\nB2", "A1\nA2")]);
+	expect(
+		CardFrontBackUtil.expand(
+			CardType.MultiLineReversed,
+			"A1\nA2\n??\nB1\nB2",
+			DEFAULT_SETTINGS,
+		),
+	).toEqual([new CardFrontBack("A1\nA2", "B1\nB2"), new CardFrontBack("B1\nB2", "A1\nA2")]);
 });
 
 test("CardType.Cloze", () => {
-    const clozeFormatter = new QuestionTypeClozeFormatter();
+	const clozeFormatter = new QuestionTypeClozeFormatter();
 
-    expect(
-        CardFrontBackUtil.expand(
-            CardType.Cloze,
-            "This is a very ==interesting== test",
-            DEFAULT_SETTINGS,
-        ),
-    ).toEqual([
-        new CardFrontBack(
-            "This is a very " + clozeFormatter.asking() + " test",
-            "This is a very " + clozeFormatter.showingAnswer("interesting") + " test",
-        ),
-    ]);
+	expect(
+		CardFrontBackUtil.expand(
+			CardType.Cloze,
+			"This is a very ==interesting== test",
+			DEFAULT_SETTINGS,
+		),
+	).toEqual([
+		new CardFrontBack(
+			"This is a very " + clozeFormatter.asking() + " test",
+			"This is a very " + clozeFormatter.showingAnswer("interesting") + " test",
+		),
+	]);
 
-    const settings2: SRSettings = DEFAULT_SETTINGS;
-    settings2.clozePatterns = [
-        "==[123;;]answer[;;hint]==",
-        "**[123;;]answer[;;hint]**",
-        "{{[123;;]answer[;;hint]}}",
-    ];
+	const settings2: SRSettings = DEFAULT_SETTINGS;
+	settings2.clozePatterns = [
+		"==[123;;]answer[;;hint]==",
+		"**[123;;]answer[;;hint]**",
+		"{{[123;;]answer[;;hint]}}",
+	];
 
-    expect(
-        CardFrontBackUtil.expand(CardType.Cloze, "This is a very **interesting** test", settings2),
-    ).toEqual([
-        new CardFrontBack(
-            "This is a very " + clozeFormatter.asking() + " test",
-            "This is a very " + clozeFormatter.showingAnswer("interesting") + " test",
-        ),
-    ]);
+	expect(
+		CardFrontBackUtil.expand(CardType.Cloze, "This is a very **interesting** test", settings2),
+	).toEqual([
+		new CardFrontBack(
+			"This is a very " + clozeFormatter.asking() + " test",
+			"This is a very " + clozeFormatter.showingAnswer("interesting") + " test",
+		),
+	]);
 
-    expect(
-        CardFrontBackUtil.expand(CardType.Cloze, "This is a very {{interesting}} test", settings2),
-    ).toEqual([
-        new CardFrontBack(
-            "This is a very " + clozeFormatter.asking() + " test",
-            "This is a very " + clozeFormatter.showingAnswer("interesting") + " test",
-        ),
-    ]);
+	expect(
+		CardFrontBackUtil.expand(CardType.Cloze, "This is a very {{interesting}} test", settings2),
+	).toEqual([
+		new CardFrontBack(
+			"This is a very " + clozeFormatter.asking() + " test",
+			"This is a very " + clozeFormatter.showingAnswer("interesting") + " test",
+		),
+	]);
 
-    expect(
-        CardFrontBackUtil.expand(
-            CardType.Cloze,
-            "This is a really very {{interesting}} and ==fascinating== and **great** test",
-            settings2,
-        ),
-    ).toEqual([
-        new CardFrontBack(
-            "This is a really very interesting and <span style='color:#2196f3'>[...]</span> and great test",
-            "This is a really very interesting and <span style='color:#2196f3'>fascinating</span> and great test",
-        ),
-        new CardFrontBack(
-            "This is a really very interesting and fascinating and <span style='color:#2196f3'>[...]</span> test",
-            "This is a really very interesting and fascinating and <span style='color:#2196f3'>great</span> test",
-        ),
-        new CardFrontBack(
-            "This is a really very <span style='color:#2196f3'>[...]</span> and fascinating and great test",
-            "This is a really very <span style='color:#2196f3'>interesting</span> and fascinating and great test",
-        ),
-    ]);
+	expect(
+		CardFrontBackUtil.expand(
+			CardType.Cloze,
+			"This is a really very {{interesting}} and ==fascinating== and **great** test",
+			settings2,
+		),
+	).toEqual([
+		new CardFrontBack(
+			"This is a really very interesting and <span style='color:#2196f3'>[...]</span> and great test",
+			"This is a really very interesting and <span style='color:#2196f3'>fascinating</span> and great test",
+		),
+		new CardFrontBack(
+			"This is a really very interesting and fascinating and <span style='color:#2196f3'>[...]</span> test",
+			"This is a really very interesting and fascinating and <span style='color:#2196f3'>great</span> test",
+		),
+		new CardFrontBack(
+			"This is a really very <span style='color:#2196f3'>[...]</span> and fascinating and great test",
+			"This is a really very <span style='color:#2196f3'>interesting</span> and fascinating and great test",
+		),
+	]);
+});
+describe("CardType.Callout", () => {
+	test("Basic", () => {
+		expect(
+			CardFrontBackUtil.expand(
+				CardType.Callout,
+				">[!Question] F1\nB1\nB2",
+				DEFAULT_SETTINGS,
+			),
+		).toEqual([new CardFrontBack("F1", "B1\nB2")]);
+	});
+	test("Default Fold Open", () => {
+		expect(
+			CardFrontBackUtil.expand(
+				CardType.Callout,
+				">[!Question]+ F1\nB1\nB2",
+				DEFAULT_SETTINGS,
+			),
+		).toEqual([new CardFrontBack("F1", "B1\nB2")]);
+	});
+	test("Default Fold Close", () => {
+		expect(
+			CardFrontBackUtil.expand(
+				CardType.Callout,
+				">[!Question]- F1\nB1\nB2",
+				DEFAULT_SETTINGS,
+			),
+		).toEqual([new CardFrontBack("F1", "B1\nB2")]);
+	});
+	test("Lowercase label", () => {
+		expect(
+			CardFrontBackUtil.expand(
+				CardType.Callout,
+				">[!question]+ F1\nB1\nB2",
+				DEFAULT_SETTINGS,
+			),
+		).toEqual([new CardFrontBack("F1", "B1\nB2")]);
+	});
 });
