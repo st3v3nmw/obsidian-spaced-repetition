657005ed66b3c5759275a1080dfd7fa2a46c0ae7
currently-coding <165482223+currently-coding@users.noreply.github.com>
2025-06-28 06:58:16 +0200

    added callouts to settings and options with defaults

--- src/settings.ts
+++ src/settings.ts
@@ -6,193 +6,197 @@ import { t } from "src/lang/helpers";
 import { pathMatchesPattern } from "src/utils/fs";
 
 export interface SRSettings {
-    // flashcards
-    flashcardTags: string[];
-    convertFoldersToDecks: boolean;
-    burySiblingCards: boolean;
-    randomizeCardOrder: boolean;
-    flashcardCardOrder: string;
-    flashcardDeckOrder: string;
-    convertHighlightsToClozes: boolean;
-    convertBoldTextToClozes: boolean;
-    convertCurlyBracketsToClozes: boolean;
-    clozePatterns: string[];
-    singleLineCardSeparator: string;
-    singleLineReversedCardSeparator: string;
-    multilineCardSeparator: string;
-    multilineReversedCardSeparator: string;
-    multilineCardEndMarker: string;
-    editLaterTag: string;
+	// flashcards
+	flashcardTags: string[];
+	convertFoldersToDecks: boolean;
+	burySiblingCards: boolean;
+	randomizeCardOrder: boolean;
+	flashcardCardOrder: string;
+	flashcardDeckOrder: string;
+	convertHighlightsToClozes: boolean;
+	convertBoldTextToClozes: boolean;
+	convertCurlyBracketsToClozes: boolean;
+	clozePatterns: string[];
+	singleLineCardSeparator: string;
+	singleLineReversedCardSeparator: string;
+	multilineCardSeparator: string;
+	multilineReversedCardSeparator: string;
+	multilineCardEndMarker: string;
+	calloutCardMarker: string,
+	calloutLineMarker: string
+	editLaterTag: string;
 
-    // notes
-    enableNoteReviewPaneOnStartup: boolean;
-    tagsToReview: string[];
-    noteFoldersToIgnore: string[];
-    openRandomNote: boolean;
-    autoNextNote: boolean;
-    disableFileMenuReviewOptions: boolean;
-    maxNDaysNotesReviewQueue: number;
+	// notes
+	enableNoteReviewPaneOnStartup: boolean;
+	tagsToReview: string[];
+	noteFoldersToIgnore: string[];
+	openRandomNote: boolean;
+	autoNextNote: boolean;
+	disableFileMenuReviewOptions: boolean;
+	maxNDaysNotesReviewQueue: number;
 
-    // UI preferences
-    showRibbonIcon: boolean;
-    showStatusBar: boolean;
-    initiallyExpandAllSubdecksInTree: boolean;
-    showContextInCards: boolean;
-    showIntervalInReviewButtons: boolean;
-    flashcardHeightPercentage: number;
-    flashcardWidthPercentage: number;
-    flashcardEasyText: string;
-    flashcardGoodText: string;
-    flashcardHardText: string;
-    reviewButtonDelay: number;
-    openViewInNewTab: boolean;
+	// UI preferences
+	showRibbonIcon: boolean;
+	showStatusBar: boolean;
+	initiallyExpandAllSubdecksInTree: boolean;
+	showContextInCards: boolean;
+	showIntervalInReviewButtons: boolean;
+	flashcardHeightPercentage: number;
+	flashcardWidthPercentage: number;
+	flashcardEasyText: string;
+	flashcardGoodText: string;
+	flashcardHardText: string;
+	reviewButtonDelay: number;
+	openViewInNewTab: boolean;
 
-    // algorithm
-    algorithm: string;
-    baseEase: number;
-    lapsesIntervalChange: number;
-    easyBonus: number;
-    loadBalance: boolean;
-    maximumInterval: number;
-    maxLinkFactor: number;
+	// algorithm
+	algorithm: string;
+	baseEase: number;
+	lapsesIntervalChange: number;
+	easyBonus: number;
+	loadBalance: boolean;
+	maximumInterval: number;
+	maxLinkFactor: number;
 
-    // storage
-    dataStore: string;
-    cardCommentOnSameLine: boolean;
+	// storage
+	dataStore: string;
+	cardCommentOnSameLine: boolean;
 
-    // logging
-    showSchedulingDebugMessages: boolean;
-    showParserDebugMessages: boolean;
+	// logging
+	showSchedulingDebugMessages: boolean;
+	showParserDebugMessages: boolean;
 }
 
 export const DEFAULT_SETTINGS: SRSettings = {
-    // flashcards
-    flashcardTags: ["#flashcards"],
-    convertFoldersToDecks: false,
-    burySiblingCards: false,
-    randomizeCardOrder: null,
-    flashcardCardOrder: "DueFirstRandom",
-    flashcardDeckOrder: "PrevDeckComplete_Sequential",
-    convertHighlightsToClozes: true,
-    convertBoldTextToClozes: false,
-    convertCurlyBracketsToClozes: false,
-    clozePatterns: ["==[123;;]answer[;;hint]=="],
-    singleLineCardSeparator: "::",
-    singleLineReversedCardSeparator: ":::",
-    multilineCardSeparator: "?",
-    multilineReversedCardSeparator: "??",
-    multilineCardEndMarker: "",
-    editLaterTag: "#edit-later",
+	// flashcards
+	flashcardTags: ["#flashcards"],
+	convertFoldersToDecks: false,
+	burySiblingCards: false,
+	randomizeCardOrder: null,
+	flashcardCardOrder: "DueFirstRandom",
+	flashcardDeckOrder: "PrevDeckComplete_Sequential",
+	convertHighlightsToClozes: true,
+	convertBoldTextToClozes: false,
+	convertCurlyBracketsToClozes: false,
+	clozePatterns: ["==[123;;]answer[;;hint]=="],
+	singleLineCardSeparator: "::",
+	singleLineReversedCardSeparator: ":::",
+	multilineCardSeparator: "?",
+	multilineReversedCardSeparator: "??",
+	multilineCardEndMarker: "",
+	calloutCardMarker: ">[!Question]",
+	calloutLineMarker: ">",
+	editLaterTag: "#edit-later",
 
-    // notes
-    enableNoteReviewPaneOnStartup: true,
-    tagsToReview: ["#review"],
-    noteFoldersToIgnore: ["**/*.excalidraw.md"],
-    openRandomNote: false,
-    autoNextNote: false,
-    disableFileMenuReviewOptions: false,
-    maxNDaysNotesReviewQueue: 365,
+	// notes
+	enableNoteReviewPaneOnStartup: true,
+	tagsToReview: ["#review"],
+	noteFoldersToIgnore: ["**/*.excalidraw.md"],
+	openRandomNote: false,
+	autoNextNote: false,
+	disableFileMenuReviewOptions: false,
+	maxNDaysNotesReviewQueue: 365,
 
-    // UI settings
-    showRibbonIcon: true,
-    showStatusBar: true,
-    initiallyExpandAllSubdecksInTree: false,
-    showContextInCards: true,
-    showIntervalInReviewButtons: true,
-    flashcardHeightPercentage: Platform.isMobile ? 100 : 80,
-    flashcardWidthPercentage: Platform.isMobile ? 100 : 40,
-    flashcardEasyText: t("EASY"),
-    flashcardGoodText: t("GOOD"),
-    flashcardHardText: t("HARD"),
-    reviewButtonDelay: 0,
-    openViewInNewTab: false,
+	// UI settings
+	showRibbonIcon: true,
+	showStatusBar: true,
+	initiallyExpandAllSubdecksInTree: false,
+	showContextInCards: true,
+	showIntervalInReviewButtons: true,
+	flashcardHeightPercentage: Platform.isMobile ? 100 : 80,
+	flashcardWidthPercentage: Platform.isMobile ? 100 : 40,
+	flashcardEasyText: t("EASY"),
+	flashcardGoodText: t("GOOD"),
+	flashcardHardText: t("HARD"),
+	reviewButtonDelay: 0,
+	openViewInNewTab: false,
 
-    // algorithm
-    algorithm: Algorithm.SM_2_OSR,
-    baseEase: 250,
-    lapsesIntervalChange: 0.5,
-    easyBonus: 1.3,
-    loadBalance: true,
-    maximumInterval: 36525,
-    maxLinkFactor: 1.0,
+	// algorithm
+	algorithm: Algorithm.SM_2_OSR,
+	baseEase: 250,
+	lapsesIntervalChange: 0.5,
+	easyBonus: 1.3,
+	loadBalance: true,
+	maximumInterval: 36525,
+	maxLinkFactor: 1.0,
 
-    // storage
-    dataStore: DataStoreName.NOTES,
-    cardCommentOnSameLine: false,
+	// storage
+	dataStore: DataStoreName.NOTES,
+	cardCommentOnSameLine: false,
 
-    // logging
-    showSchedulingDebugMessages: false,
-    showParserDebugMessages: false,
+	// logging
+	showSchedulingDebugMessages: false,
+	showParserDebugMessages: false,
 };
 
 export function upgradeSettings(settings: SRSettings) {
-    if (
-        settings.randomizeCardOrder != null &&
-        settings.flashcardCardOrder == null &&
-        settings.flashcardDeckOrder == null
-    ) {
-        settings.flashcardCardOrder = settings.randomizeCardOrder
-            ? "DueFirstRandom"
-            : "DueFirstSequential";
-        settings.flashcardDeckOrder = "PrevDeckComplete_Sequential";
+	if (
+		settings.randomizeCardOrder != null &&
+		settings.flashcardCardOrder == null &&
+		settings.flashcardDeckOrder == null
+	) {
+		settings.flashcardCardOrder = settings.randomizeCardOrder
+			? "DueFirstRandom"
+			: "DueFirstSequential";
+		settings.flashcardDeckOrder = "PrevDeckComplete_Sequential";
 
-        // After the upgrade, we don't need the old attribute any more
-        settings.randomizeCardOrder = null;
-    }
+		// After the upgrade, we don't need the old attribute any more
+		settings.randomizeCardOrder = null;
+	}
 
-    if (settings.clozePatterns == null) {
-        settings.clozePatterns = [];
+	if (settings.clozePatterns == null) {
+		settings.clozePatterns = [];
 
-        if (settings.convertHighlightsToClozes)
-            settings.clozePatterns.push("==[123;;]answer[;;hint]==");
+		if (settings.convertHighlightsToClozes)
+			settings.clozePatterns.push("==[123;;]answer[;;hint]==");
 
-        if (settings.convertBoldTextToClozes)
-            settings.clozePatterns.push("**[123;;]answer[;;hint]**");
+		if (settings.convertBoldTextToClozes)
+			settings.clozePatterns.push("**[123;;]answer[;;hint]**");
 
-        if (settings.convertCurlyBracketsToClozes)
-            settings.clozePatterns.push("{{[123;;]answer[;;hint]}}");
-    }
+		if (settings.convertCurlyBracketsToClozes)
+			settings.clozePatterns.push("{{[123;;]answer[;;hint]}}");
+	}
 }
 
 export class SettingsUtil {
-    static isFlashcardTag(settings: SRSettings, tag: string): boolean {
-        return SettingsUtil.isTagInList(settings.flashcardTags, tag);
-    }
+	static isFlashcardTag(settings: SRSettings, tag: string): boolean {
+		return SettingsUtil.isTagInList(settings.flashcardTags, tag);
+	}
 
-    static isPathInNoteIgnoreFolder(settings: SRSettings, path: string): boolean {
-        return settings.noteFoldersToIgnore.some((folder) => pathMatchesPattern(path, folder));
-    }
+	static isPathInNoteIgnoreFolder(settings: SRSettings, path: string): boolean {
+		return settings.noteFoldersToIgnore.some((folder) => pathMatchesPattern(path, folder));
+	}
 
-    static isAnyTagANoteReviewTag(settings: SRSettings, tags: string[]): boolean {
-        for (const tag of tags) {
-            if (
-                settings.tagsToReview.some(
-                    (tagToReview) => tag === tagToReview || tag.startsWith(tagToReview + "/"),
-                )
-            ) {
-                return true;
-            }
-        }
-        return false;
-    }
+	static isAnyTagANoteReviewTag(settings: SRSettings, tags: string[]): boolean {
+		for (const tag of tags) {
+			if (
+				settings.tagsToReview.some(
+					(tagToReview) => tag === tagToReview || tag.startsWith(tagToReview + "/"),
+				)
+			) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    // Given a list of tags, return the subset that is in settings.tagsToReview
-    static filterForNoteReviewTag(settings: SRSettings, tags: string[]): string[] {
-        const result: string[] = [];
-        for (const tagToReview of settings.tagsToReview) {
-            if (tags.some((tag) => tag === tagToReview || tag.startsWith(tagToReview + "/"))) {
-                result.push(tagToReview);
-            }
-        }
-        return result;
-    }
+	// Given a list of tags, return the subset that is in settings.tagsToReview
+	static filterForNoteReviewTag(settings: SRSettings, tags: string[]): string[] {
+		const result: string[] = [];
+		for (const tagToReview of settings.tagsToReview) {
+			if (tags.some((tag) => tag === tagToReview || tag.startsWith(tagToReview + "/"))) {
+				result.push(tagToReview);
+			}
+		}
+		return result;
+	}
 
-    private static isTagInList(tagList: string[], tag: string): boolean {
-        for (const tagFromList of tagList) {
-            if (tag === tagFromList || tag.startsWith(tagFromList + "/")) {
-                return true;
-            }
-        }
-        return false;
-    }
+	private static isTagInList(tagList: string[], tag: string): boolean {
+		for (const tagFromList of tagList) {
+			if (tag === tagFromList || tag.startsWith(tagFromList + "/")) {
+				return true;
+			}
+		}
+		return false;
+	}
 }
