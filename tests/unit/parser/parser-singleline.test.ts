102d80312a32a97f82a2d34e39c3e917c14a4d39
Kyle Klus <kyle.klus.2@gmail.com>
2026-03-04 12:12:30 +0100

    FEAT: Improved tests

--- tests/unit/parser/parser-singleline.test.ts
+++ tests/unit/parser/parser-singleline.test.ts
@@ -2,58 +2,64 @@ import { CardType } from "src/card/questions/question";
 
 import { parserOptions, parseT } from "../helpers/unit-test-parser-helper";
 
-test("Test parsing of single line basic cards", () => {
+const execInlineCardsTestWithSeparator = (separator: string) => {
+    // Same tests apply for both single line basic and singleline reversed cards
+
     // standard symbols
-    expect(parseT(["Question::Answer"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, "Question::Answer", 0, 0],
+    expect(parseT(["Question" + separator + "Answer"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question" + separator + "Answer"].join("\n"), 0, 0],
     ]);
 
-    expect(parseT(["Question::Answer <!--SR:2021-08-11,4,270-->"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question::Answer <!--SR:2021-08-11,4,270-->"].join("\n"), 0, 0],
+    expect(parseT(["Question" + separator + "Answer <!--SR:2021-08-11,4,270-->"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question" + separator + "Answer <!--SR:2021-08-11,4,270-->"].join("\n"), 0, 0],
     ]);
 
-    expect(parseT(["Question::Answer", "<!--SR:!2021-08-11,4,270-->"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question::Answer", "<!--SR:!2021-08-11,4,270-->"].join("\n"), 0, 1],
+    expect(parseT(["Question" + separator + "Answer", "<!--SR:!2021-08-11,4,270-->"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question" + separator + "Answer", "<!--SR:!2021-08-11,4,270-->"].join("\n"), 0, 1],
     ]);
 
-    expect(parseT(["Some text before", "Question ::Answer"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer"].join("\n"), 1, 1],
+    expect(parseT(["Some text before", "Question " + separator + "Answer"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer"].join("\n"), 1, 1],
     ]);
-    expect(parseT(["Some text before", "Question ::Answer <!--SR:2021-08-11,4,270-->"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer <!--SR:2021-08-11,4,270-->"].join("\n"), 1, 1],
+    expect(parseT(["Some text before", "Question " + separator + "Answer <!--SR:2021-08-11,4,270-->"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer <!--SR:2021-08-11,4,270-->"].join("\n"), 1, 1],
     ]);
-    expect(parseT(["Some text before", "Question ::Answer", "<!--SR:2021-08-11,4,270-->"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer", "<!--SR:2021-08-11,4,270-->"].join("\n"), 1, 2],
-    ]);
-
-    expect(parseT(["Question ::Answer", "Some text after"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer"].join("\n"), 0, 0],
-    ]);
-    expect(parseT(["Question ::Answer <!--SR:2021-08-11,4,270-->", "Some text after"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer <!--SR:2021-08-11,4,270-->"].join("\n"), 0, 0],
-    ]);
-    expect(parseT(["Question ::Answer", " <!--SR:2021-08-11,4,270-->", "Some text after"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer", " <!--SR:2021-08-11,4,270-->"].join("\n"), 0, 1],
+    expect(parseT(["Some text before", "Question " + separator + "Answer", "<!--SR:2021-08-11,4,270-->"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer", "<!--SR:2021-08-11,4,270-->"].join("\n"), 1, 2],
     ]);
 
-    expect(parseT(["Some text before", "Question ::Answer", "Some text after"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer"].join("\n"), 1, 1],
+    expect(parseT(["Question " + separator + "Answer", "Some text after"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer"].join("\n"), 0, 0],
     ]);
-    expect(parseT(["Some text before", "Question ::Answer <!--SR:2021-08-11,4,270-->", "Some text after"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer <!--SR:2021-08-11,4,270-->"].join("\n"), 1, 1],
+    expect(parseT(["Question " + separator + "Answer <!--SR:2021-08-11,4,270-->", "Some text after"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer <!--SR:2021-08-11,4,270-->"].join("\n"), 0, 0],
     ]);
-    expect(parseT(["Some text before", "Question ::Answer", " <!--SR:2021-08-11,4,270-->", "Some text after"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Question ::Answer", " <!--SR:2021-08-11,4,270-->"].join("\n"), 1, 2],
-    ]);
-
-    expect(parseT(["#Title", "Q1::A1", "Q2:: A2"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["Q1::A1"].join("\n"), 2, 2],
-        [CardType.SingleLineBasic, ["Q2:: A2"].join("\n"), 3, 3],
+    expect(parseT(["Question " + separator + "Answer", " <!--SR:2021-08-11,4,270-->", "Some text after"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer", " <!--SR:2021-08-11,4,270-->"].join("\n"), 0, 1],
     ]);
 
-    expect(parseT(["#flashcards/science Question ::Answer"].join("\n"), parserOptions)).toEqual([
-        [CardType.SingleLineBasic, ["#flashcards/science Question ::Answer"].join("\n"), 0, 0],
+    expect(parseT(["Some text before", "Question " + separator + "Answer", "Some text after"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer"].join("\n"), 1, 1],
     ]);
+    expect(parseT(["Some text before", "Question " + separator + "Answer <!--SR:2021-08-11,4,270-->", "Some text after"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer <!--SR:2021-08-11,4,270-->"].join("\n"), 1, 1],
+    ]);
+    expect(parseT(["Some text before", "Question " + separator + "Answer", " <!--SR:2021-08-11,4,270-->", "Some text after"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Question " + separator + "Answer", " <!--SR:2021-08-11,4,270-->"].join("\n"), 1, 2],
+    ]);
+
+    expect(parseT(["#Title", "Q1" + separator + "A1", "Q2" + separator + " A2"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Q1" + separator + "A1"].join("\n"), 2, 2],
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["Q2" + separator + " A2"].join("\n"), 3, 3],
+    ]);
+
+    expect(parseT(["#flashcards/science Question " + separator + "Answer"].join("\n"), parserOptions)).toEqual([
+        [separator === "::" ? CardType.SingleLineBasic : CardType.SingleLineReversed, ["#flashcards/science Question " + separator + "Answer"].join("\n"), 0, 0],
+    ]);
+};
+
+test("Test parsing of single line basic cards", () => {
+    execInlineCardsTestWithSeparator("::");
 
     // custom symbols
     expect(
@@ -92,16 +98,7 @@ test("Test parsing of single line basic cards", () => {
 
 test("Test parsing of single line reversed cards", () => {
     // standard symbols
-    expect(parseT("Question:::Answer", parserOptions)).toEqual([
-        [CardType.SingleLineReversed, "Question:::Answer", 0, 0],
-    ]);
-    expect(parseT("Some text before\nQuestion :::Answer", parserOptions)).toEqual([
-        [CardType.SingleLineReversed, "Question :::Answer", 1, 1],
-    ]);
-    expect(parseT("#Title\n\nQ1:::A1\nQ2::: A2", parserOptions)).toEqual([
-        [CardType.SingleLineReversed, "Q1:::A1", 2, 2],
-        [CardType.SingleLineReversed, "Q2::: A2", 3, 3],
-    ]);
+    execInlineCardsTestWithSeparator(":::");
 
     // custom symbols
     expect(
