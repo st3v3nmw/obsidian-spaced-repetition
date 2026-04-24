2bfd65d43f83b22b09cc4d77c8d83443c0a9b2db
Kyle Klus <kyle.klus.2@gmail.com>
2026-04-24 19:00:03 +0200

    FIX: Fixed that some embeds weren't loading

--- src/utils/renderers.ts
+++ src/utils/renderers.ts
@@ -1,6 +1,5 @@
-import { App, MarkdownRenderChild, MarkdownRenderer, TFile } from "obsidian";
+import { App, MarkdownRenderChild, MarkdownRenderer } from "obsidian";
 
-import { AUDIO_FORMATS, IMAGE_FORMATS, VIDEO_FORMATS } from "src/constants";
 import SRPlugin from "src/main";
 import { TextDirection } from "src/utils/strings";
 
@@ -51,7 +50,6 @@ export class RenderMarkdownWrapper {
 
             (el as HTMLAnchorElement).addEventListener("mouseover", (ev: Event) => {
                 const href = el.getAttr("href") || el.getAttr("data-href");
-                console.log(href);
                 if (href) {
                     this.app.workspace.trigger("hover-link", {
                         event: ev,
@@ -65,86 +63,5 @@ export class RenderMarkdownWrapper {
                 return false;
             });
         });
-
-        el.findAll(".internal-embed").forEach((el) => {
-            const link = this.parseLink(el.getAttribute("src"));
-
-            // file does not exist, display dead link
-            if (!link.target) {
-                el.innerText = link.text;
-            } else if (link.target instanceof TFile) {
-                if (link.target.extension !== "md") {
-                    this.embedMediaFile(el, link.target);
-                } else {
-                    // We get here if there is a transclusion link, such as "![[Test Embed]]"
-                    // In version 1.12.4 and earlier we used the deprecated Obsidian MarkdownRenderer.renderMarkdown() and we
-                    // needed to have our own method "renderTransclude()" that loaded the referenced file and rendered it.
-                    // In version 1.12.5, we started using MarkdownRenderer.render() instead, which does this automatically.
-                }
-            }
-        });
-    }
-
-    private parseLink(src: string) {
-        const linkComponentsRegex =
-            /^(?<file>[^#^]+)?(?:#(?!\^)(?<heading>.+)|#\^(?<blockId>.+)|#)?$/;
-        const matched = typeof src === "string" && src.match(linkComponentsRegex);
-        const file = matched.groups.file || this.notePath;
-        const target = this.plugin.app.metadataCache.getFirstLinkpathDest(file, this.notePath);
-        return {
-            text: matched[0],
-            file: matched.groups.file,
-            heading: matched.groups.heading,
-            blockId: matched.groups.blockId,
-            target: target,
-        };
-    }
-
-    private embedMediaFile(el: HTMLElement, target: TFile) {
-        el.innerText = "";
-        if (IMAGE_FORMATS.includes(target.extension)) {
-            el.createEl(
-                "img",
-                {
-                    attr: {
-                        src: this.plugin.app.vault.getResourcePath(target),
-                    },
-                },
-                (img) => {
-                    if (el.hasAttribute("width"))
-                        img.setAttribute("width", el.getAttribute("width"));
-                    else img.setAttribute("width", "100%");
-                    if (el.hasAttribute("alt")) img.setAttribute("alt", el.getAttribute("alt"));
-                    el.addEventListener(
-                        "click",
-                        (ev) =>
-                            ((ev.target as HTMLElement).style.minWidth =
-                                (ev.target as HTMLElement).style.minWidth === "100%"
-                                    ? null
-                                    : "100%"),
-                    );
-                },
-            );
-            el.addClasses(["image-embed", "is-loaded"]);
-        } else if (
-            AUDIO_FORMATS.includes(target.extension) ||
-            VIDEO_FORMATS.includes(target.extension)
-        ) {
-            el.createEl(
-                AUDIO_FORMATS.includes(target.extension) ? "audio" : "video",
-                {
-                    attr: {
-                        controls: "",
-                        src: this.plugin.app.vault.getResourcePath(target),
-                    },
-                },
-                (audio) => {
-                    if (el.hasAttribute("alt")) audio.setAttribute("alt", el.getAttribute("alt"));
-                },
-            );
-            el.addClasses(["media-embed", "is-loaded"]);
-        } else {
-            el.innerText = target.path;
-        }
     }
 }
